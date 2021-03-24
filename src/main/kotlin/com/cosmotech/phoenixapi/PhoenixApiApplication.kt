c93f672eb8026d56b23c1c1d94efbc43f92bdb56
Armel Soro <armel.soro@cosmotech.com>
2021-03-24 17:17:50 +0100

    Initial commit

---
+++ src/main/kotlin/com/cosmotech/phoenixapi/PhoenixApiApplication.kt
@@ -0,0 +1,11 @@
+package com.cosmotech.phoenixapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhoenixApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhoenixApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/cosmotech/phoenixapi/PhoenixApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cosmotech.phoenixapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhoenixApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
