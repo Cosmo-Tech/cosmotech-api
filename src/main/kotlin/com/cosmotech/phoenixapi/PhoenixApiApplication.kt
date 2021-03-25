8e4f1171f286148aefe54200e599128c630b55a1
Armel Soro <armel.soro@cosmotech.com>
2021-03-25 18:03:39 +0100

    Scan all 'com.cosmotech' packages for Spring components in the parent API project

--- src/main/kotlin/com/cosmotech/phoenixapi/PhoenixApiApplication.kt
+++ src/main/kotlin/com/cosmotech/phoenixapi/PhoenixApiApplication.kt
@@ -2,8 +2,10 @@ package com.cosmotech.phoenixapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.cosmotech"])
 class PhoenixApiApplication
 
 fun main(args: Array<String>) {
