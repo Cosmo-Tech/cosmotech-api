882dbd78a6063d35aa7eb0aed48384fb24b46a9d
Armel Soro <armel.soro@cosmotech.com>
2021-03-26 16:06:26 +0100

    [user] Add User module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,8 @@
 rootProject.name = "cosmotech-api"
 include("common")
 include("organization")
+include("user")
 
 project(":common").name = "cosmotech-api-common"
 project(":organization").name = "cosmotech-organization-api"
+project(":user").name = "cosmotech-user-api"
