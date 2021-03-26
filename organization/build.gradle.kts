dcebe7c76f5b9f7e56ade1014029bb45eef479a7
Armel Soro <armel.soro@cosmotech.com>
2021-03-26 13:57:06 +0100

    [organization] Disable generation of API Tests
    The OpenAPI generator generates test classes that expect the
    Service Implementation to be present in the 'apiPackage' package,
    which is not the case when serviceInterface is true.
    We will write our own tests instead.

--- organization/build.gradle.kts
+++ organization/build.gradle.kts
@@ -1,6 +1,5 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 import org.openapitools.generator.gradle.plugin.tasks.ValidateTask
-import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
    id("org.openapi.generator")
@@ -14,11 +13,9 @@ sourceSets {
    main {
       java.srcDirs("$buildDir/generated-sources/openapi/src/main/kotlin")
    }
-//   test {
-   //Excluded because the OpenAPI Generator generates test classes that expect the Service Implementation to be
-   //present, which is not the case when serviceInterface is true
-//      java.srcDirs("$buildDir/generated-sources/openapi/src/test/kotlin")
-//   }
+   test {
+      java.srcDirs("$buildDir/generated-sources/openapi/src/test/kotlin")
+   }
 }
 
 tasks.getByName<ValidateTask>("openApiValidate") {
@@ -31,10 +28,14 @@ tasks.getByName<GenerateTask>("openApiGenerate") {
    generatorName.set("kotlin-spring")
    apiPackage.set("com.cosmotech.organization.api")
    modelPackage.set("com.cosmotech.organization.domain")
-//   globalProperties.set(mapOf(
-//           "apis" to "",
-//           "models" to ""
-//   ))
+   globalProperties.set(mapOf(
+           "apiDocs" to "true",
+           // Excluded because the OpenAPI Generator generates test classes that expect the
+           // Service Implementation to be present in the 'apiPackage' package,
+           // which is not the case when serviceInterface is true.
+           // We will write our own tests instead.
+           "apiTests" to "false"
+   ))
    additionalProperties.set(mapOf(
            "title" to "Cosmo Tech Organization Manager API",
            "basePackage" to "com.cosmotech.organization",
