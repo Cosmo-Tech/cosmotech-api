c5a7fa1ebf65a7d8c439d3ac2161f2b9a11d79ae
Vincent Carluer <vcarluer@gmail.com>
2021-03-31 11:54:38 +0200

    no more connector patch + spotless

--- connector/src/main/kotlin/com/cosmotech/connector/ConnectorServiceImpl.kt
+++ connector/src/main/kotlin/com/cosmotech/connector/ConnectorServiceImpl.kt
@@ -6,10 +6,7 @@ package com.cosmotech.connector
 import com.cosmotech.api.AbstractPhoenixService
 import com.cosmotech.connector.api.ConnectorsApiService
 import com.cosmotech.connector.domain.Connector
-import com.cosmotech.connector.domain.ConnectorParameterGroup
-import java.io.BufferedReader
 import org.springframework.stereotype.Service
-import org.springframework.util.FileCopyUtils
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.introspector.BeanAccess
 
@@ -29,16 +26,12 @@ class ConnectorServiceImpl : AbstractPhoenixService(), ConnectorsApiService {
 
   override fun uploadConnector(body: org.springframework.core.io.Resource): Connector {
     val yaml = Yaml()
-    yaml.setBeanAccess(BeanAccess.FIELD);
+    yaml.setBeanAccess(BeanAccess.FIELD)
     val connector = yaml.loadAs(body.getInputStream(), Connector::class.java)
     return connector
   }
 
   override fun unregisterConnector(connectorId: String): Connector {
-    TODO("Not yet implemented")
-  }
-
-  override fun updateConnector(connectorId: String, connector: Connector): Connector {
     TODO("Not yet implemented")
   }
 }
