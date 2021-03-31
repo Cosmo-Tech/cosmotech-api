b24e1cfe3e82b74211c68d3ab4399442169b58c0
Vincent Carluer <vcarluer@gmail.com>
2021-03-31 18:24:49 +0200

    required & readOnly properties

--- user/src/main/kotlin/com/cosmotech/user/UserServiceImpl.kt
+++ user/src/main/kotlin/com/cosmotech/user/UserServiceImpl.kt
@@ -6,27 +6,28 @@ package com.cosmotech.user
 import com.cosmotech.api.AbstractPhoenixService
 import com.cosmotech.user.api.UsersApiService
 import com.cosmotech.user.domain.User
+import com.cosmotech.user.domain.UserDetails
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl : AbstractPhoenixService(), UsersApiService {
-  override fun findAllUsers(): List<User> {
+  override fun findAllUsers(): List<UserDetails> {
     TODO("Not yet implemented")
   }
 
-  override fun findUserById(userId: String): User {
+  override fun findUserById(userId: String): UserDetails {
     TODO("Not yet implemented")
   }
 
-  override fun registerUser(user: User): User {
+  override fun registerUser(user: User): UserDetails {
     TODO("Not yet implemented")
   }
 
-  override fun unregisterUser(userId: String): User {
+  override fun unregisterUser(userId: String): UserDetails {
     TODO("Not yet implemented")
   }
 
-  override fun updateUser(userId: String, user: User): User {
+  override fun updateUser(userId: String, user: User): UserDetails {
     TODO("Not yet implemented")
   }
 }
