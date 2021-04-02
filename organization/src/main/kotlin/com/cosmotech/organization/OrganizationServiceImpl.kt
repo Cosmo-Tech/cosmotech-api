3273315566e24a38dead6f9a630aa594d2c31531
Armel Soro <armel.soro@cosmotech.com>
2021-04-02 10:33:46 +0200

    Implement OrganizationService#registerOrganization

--- organization/src/main/kotlin/com/cosmotech/organization/OrganizationServiceImpl.kt
+++ organization/src/main/kotlin/com/cosmotech/organization/OrganizationServiceImpl.kt
@@ -2,13 +2,30 @@
 // Licensed under the MIT license.
 package com.cosmotech.organization
 
+import com.azure.cosmos.CosmosClient
+import com.azure.cosmos.models.CosmosContainerProperties
+import com.azure.cosmos.models.CosmosQueryRequestOptions
 import com.cosmotech.api.AbstractPhoenixService
 import com.cosmotech.organization.api.OrganizationApiService
 import com.cosmotech.organization.domain.Organization
+import java.util.*
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class OrganizationServiceImpl : AbstractPhoenixService(), OrganizationApiService {
+class OrganizationServiceImpl(
+    // TODO Consider using the async client instead + WebFlux
+    val cosmosClient: CosmosClient
+) : AbstractPhoenixService(), OrganizationApiService {
+
+  private val logger = LoggerFactory.getLogger(OrganizationServiceImpl::class.java)
+
+  @Value("\${csm.azure.cosmosdb.database.core}")
+  private lateinit var azureCosmosDbDatabaseCore: String
+
+  @Value("\${csm.azure.cosmosdb.database.core.organizations.container}")
+  private lateinit var azureCosmosDbDatabaseCoreOrganizationContainer: String
   override fun findAllOrganizations(): List<Organization> {
     TODO("Not yet implemented")
   }
@@ -18,7 +35,25 @@ class OrganizationServiceImpl : AbstractPhoenixService(), OrganizationApiService
   }
 
   override fun registerOrganization(organization: Organization): Organization {
-    TODO("Not yet implemented")
+    logger.trace("Registering organization : $organization")
+    val database = cosmosClient.getDatabase(azureCosmosDbDatabaseCore)
+    val organizationId = UUID.randomUUID().toString()
+    val organizationToCreate = organization.copy(id = organizationId)
+    val createItemResponse =
+        database
+            .getContainer(azureCosmosDbDatabaseCoreOrganizationContainer)
+            .createItem(organizationToCreate)
+
+    logger.trace("[registerOrganization($organization)] : createItemResponse: $createItemResponse")
+
+    database.createContainerIfNotExists(
+        CosmosContainerProperties("${organizationId}_user-data", "/userId"))
+
+    database.createContainerIfNotExists(
+        CosmosContainerProperties("${organizationId}_workspaces", "/workspaceId"))
+
+    // TODO Publish event and handle rollbacks in case of errors
+    return organizationToCreate
   }
 
   override fun unregisterOrganization(organizationId: String): Organization {
