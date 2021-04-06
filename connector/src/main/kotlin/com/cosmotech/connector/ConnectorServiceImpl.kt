29de526d52aa229ce696236d892f4afe52d3ffd2
Vincent Carluer <vcarluer@gmail.com>
2021-04-06 19:28:58 +0200

    WIP Simulator

--- connector/src/main/kotlin/com/cosmotech/connector/ConnectorServiceImpl.kt
+++ connector/src/main/kotlin/com/cosmotech/connector/ConnectorServiceImpl.kt
@@ -1,36 +1,36 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-package com.cosmotech.connector
+package com.cosmotech.simulator
 
 import com.cosmotech.api.AbstractPhoenixService
-import com.cosmotech.connector.api.ConnectorsApiService
-import com.cosmotech.connector.domain.Connector
+import com.cosmotech.simulator.api.SimulatorsApiService
+import com.cosmotech.simulator.domain.Simulator
 import org.springframework.stereotype.Service
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.introspector.BeanAccess
 
 @Service
-class ConnectorServiceImpl : AbstractPhoenixService(), ConnectorsApiService {
-  override fun findAllConnectors(): List<Connector> {
+class SimulatorServiceImpl : AbstractPhoenixService(), SimulatorsApiService {
+  override fun findAllSimulators(): List<Simulator> {
     TODO("Not yet implemented")
   }
 
-  override fun findConnectorById(connectorId: String): Connector {
+  override fun findSimulatorById(simulatorId: String): Simulator {
     TODO("Not yet implemented")
   }
 
-  override fun registerConnector(connector: Connector): Connector {
+  override fun registerSimulator(simulator: Simulator): Simulator {
     TODO("Not yet implemented")
   }
 
-  override fun uploadConnector(body: org.springframework.core.io.Resource): Connector {
+  override fun uploadSimulator(body: org.springframework.core.io.Resource): Simulator {
     val yaml = Yaml()
     yaml.setBeanAccess(BeanAccess.FIELD)
-    val connector = yaml.loadAs(body.getInputStream(), Connector::class.java)
-    return connector
+    val simulator = yaml.loadAs(body.getInputStream(), Simulator::class.java)
+    return simulator
   }
 
-  override fun unregisterConnector(connectorId: String): Connector {
+  override fun unregisterSimulator(simulatorId: String): Simulator {
     TODO("Not yet implemented")
   }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,8 @@ include("connector")
 
 include("dataset")
 
+include("simulator")
+
 project(":api").name = "cosmotech-api"
 
 project(":common").name = "cosmotech-api-common"
@@ -23,3 +25,5 @@ project(":user").name = "cosmotech-user-api"
 project(":connector").name = "cosmotech-connector-api"
 
 project(":dataset").name = "cosmotech-dataset-api"
+
+project(":simulator").name = "cosmotech-simulator-api"
