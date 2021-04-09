3f773e5ba90ac89c1b0a6f4e2a984e2da03c7bf6
Armel Soro <armel.soro@cosmotech.com>
2021-04-09 17:26:05 +0200

    WIP - Actually use Spring Boot Cosmos Starter
    Even though domain classes are not usable via repositories
    unless annotated with Cosmos Annotations, we can still
    leverage the Spring Boot AutoConfiguration classes provided,
    which also injects other useful beans (such as CosmosTemplate,
    CosmosClientBuilder and the like) to perform a bunch of
    operations, while benefiting from automatic data (un-)marshalling.

--- organization/src/main/kotlin/com/cosmotech/organization/OrganizationServiceImpl.kt
+++ organization/src/main/kotlin/com/cosmotech/organization/OrganizationServiceImpl.kt
@@ -3,14 +3,16 @@
 package com.cosmotech.organization
 
 import com.azure.cosmos.CosmosClient
+import com.azure.cosmos.CosmosClientBuilder
 import com.azure.cosmos.models.CosmosContainerProperties
-import com.azure.cosmos.models.CosmosQueryRequestOptions
+import com.azure.spring.data.cosmos.core.CosmosTemplate
 import com.cosmotech.api.AbstractPhoenixService
 import com.cosmotech.api.events.OrganizationRegistered
 import com.cosmotech.organization.api.OrganizationApiService
 import com.cosmotech.organization.domain.Organization
-import com.fasterxml.jackson.databind.node.ObjectNode
+import java.lang.IllegalStateException
 import java.util.*
+import javax.annotation.PostConstruct
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -18,55 +20,42 @@ import org.springframework.stereotype.Service
 @Service
 class OrganizationServiceImpl(
     // TODO Consider using the async client instead + WebFlux
-    val cosmosClient: CosmosClient
+    val cosmosTemplate: CosmosTemplate,
+    val cosmosClientBuilder: CosmosClientBuilder
 ) : AbstractPhoenixService(), OrganizationApiService {
 
   private val logger = LoggerFactory.getLogger(OrganizationServiceImpl::class.java)
 
-  @Value("\${csm.azure.cosmosdb.database.core}")
-  private lateinit var azureCosmosDbDatabaseCore: String
+  private var cosmosClient: CosmosClient? = null
 
   @Value("\${csm.azure.cosmosdb.database.core.organizations.container}")
-  private lateinit var azureCosmosDbDatabaseCoreOrganizationContainer: String
-  override fun findAllOrganizations(): List<Organization> {
-    return cosmosClient
-        .getDatabase(azureCosmosDbDatabaseCore)
-        .getContainer(azureCosmosDbDatabaseCoreOrganizationContainer)
-        .queryItems("SELECT * FROM c", CosmosQueryRequestOptions(), ObjectNode::class.java)
-        .map {
-          // Workaround because of the issue reported in
-          // https://github.com/Azure/azure-sdk-for-java/issues/12269
-          // Azure Cosmos SDK is not able to deserialize Kotlin data classes
-          convertTo<Organization>(it)
-        }
-        .toList()
+  private lateinit var coreOrganizationContainer: String
+  @PostConstruct
+  fun init() {
+    // TODO Consider using the async client instead
+    this.cosmosClient = cosmosClientBuilder.buildClient()
   }
 
-  override fun findOrganizationById(organizationId: String): Organization {
-    val objectNode =
-        cosmosClient
-            .getDatabase(azureCosmosDbDatabaseCore)
-            .getContainer(azureCosmosDbDatabaseCoreOrganizationContainer)
-            .queryItems(
-                "SELECT * FROM c WHERE c.id = \"$organizationId\" OFFSET 0 LIMIT 1",
-                CosmosQueryRequestOptions(),
-                ObjectNode::class.java)
-            .firstOrNull()
-    return convertTo(
-        objectNode ?: throw IllegalArgumentException("Organization not found: $organizationId"))
-  }
+  override fun findAllOrganizations() =
+      cosmosTemplate.findAll(coreOrganizationContainer, Organization::class.java).toList()
+
+  override fun findOrganizationById(organizationId: String) =
+      cosmosTemplate.findById(coreOrganizationContainer, organizationId, Organization::class.java)
+          ?: throw IllegalArgumentException("Organization not found: $organizationId")
 
   override fun registerOrganization(organization: Organization): Organization {
     logger.trace("Registering organization : $organization")
-    val database = cosmosClient.getDatabase(azureCosmosDbDatabaseCore)
-    val organizationId = UUID.randomUUID().toString()
-    val organizationToCreate = organization.copy(id = organizationId)
-    val createItemResponse =
-        database
-            .getContainer(azureCosmosDbDatabaseCoreOrganizationContainer)
-            .createItem(organizationToCreate)
 
-    logger.trace("[registerOrganization($organization)] : createItemResponse: $createItemResponse")
+    val organizationRegistered =
+        cosmosTemplate.insert(
+            coreOrganizationContainer, organization.copy(id = UUID.randomUUID().toString()))
+
+    val organizationId =
+        organizationRegistered.id
+            ?: throw IllegalStateException(
+                "No ID returned for organization registered: $organizationRegistered")
+
+    val database = cosmosClient!!.getDatabase(coreOrganizationContainer)
 
     database.createContainerIfNotExists(
         CosmosContainerProperties("${organizationId}_user-data", "/userId"))
@@ -75,9 +64,9 @@ class OrganizationServiceImpl(
         CosmosContainerProperties("${organizationId}_workspaces", "/workspaceId"))
 
     this.eventPublisher.publishEvent(OrganizationRegistered(this, organizationId))
-    // TODO Handle rollbacks in case of errors
-
-    return organizationToCreate
+    //    // TODO Handle rollbacks in case of errors
+    //
+    return organizationRegistered
   }
 
   override fun unregisterOrganization(organizationId: String): Organization {
