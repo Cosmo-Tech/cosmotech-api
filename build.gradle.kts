ba4d68cbb2195c42e84abb6b04e6887151eb17ac
Armel Soro <armel.soro@cosmotech.com>
2021-04-19 17:42:48 +0200

    Switch to a more lightweight Embedded Servlet Container like Jetty

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,10 @@ subprojects {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+      exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("javax.validation:validation-api:2.0.1.Final")
 
