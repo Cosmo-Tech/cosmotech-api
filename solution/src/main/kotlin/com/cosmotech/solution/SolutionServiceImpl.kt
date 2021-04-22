c279b49c3757302f58a5ce89c01174c5624d384a
Armel Soro <armel.soro@cosmotech.com>
2021-04-22 18:07:07 +0200

    [solution] Implement most of the SolutionService functions

--- solution/src/main/kotlin/com/cosmotech/solution/SolutionServiceImpl.kt
+++ solution/src/main/kotlin/com/cosmotech/solution/SolutionServiceImpl.kt
@@ -2,27 +2,42 @@
 // Licensed under the MIT license.
 package com.cosmotech.solution
 
-import com.cosmotech.api.AbstractPhoenixService
+import com.azure.cosmos.models.CosmosContainerProperties
+import com.cosmotech.api.AbstractCosmosBackedService
+import com.cosmotech.api.events.OrganizationRegistered
+import com.cosmotech.api.events.OrganizationUnregistered
 import com.cosmotech.solution.api.SolutionApiService
 import com.cosmotech.solution.domain.Solution
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.util.*
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 @Service
-class SolutionServiceImpl : AbstractPhoenixService(), SolutionApiService {
-  override fun findAllSolutions(organizationId: String): List<Solution> {
-    TODO("Not yet implemented")
-  }
+@ConditionalOnProperty(name = ["csm.platform.vendor"], havingValue = "azure", matchIfMissing = true)
+class SolutionServiceImpl : AbstractCosmosBackedService(), SolutionApiService {
 
-  override fun findSolutionById(organizationId: String, solutionId: String): Solution {
-    TODO("Not yet implemented")
-  }
+  override fun findAllSolutions(organizationId: String) =
+      cosmosTemplate.findAll("${organizationId}_solutions", Solution::class.java).toList()
 
-  override fun createSolution(organizationId: String, solution: Solution): Solution {
-    TODO("Not yet implemented")
-  }
+  override fun findSolutionById(organizationId: String, solutionId: String) =
+      cosmosTemplate.findById("${organizationId}_solutions", solutionId, Solution::class.java)
+          ?: throw IllegalArgumentException(
+              "Solution $solutionId not found in organization $organizationId")
+
+  override fun createSolution(organizationId: String, solution: Solution) =
+      cosmosTemplate.insert(
+          "${organizationId}_solutions", solution.copy(id = UUID.randomUUID().toString()))
+          ?: throw IllegalArgumentException("No solution returned in response: $solution")
 
   override fun deleteSolution(organizationId: String, solutionId: String): Solution {
-    TODO("Not yet implemented")
+    val solution = findSolutionById(organizationId, solutionId)
+    cosmosTemplate.deleteEntity("${organizationId}_solutions", solution)
+    return solution
   }
 
   override fun updateSolution(
@@ -33,11 +48,27 @@ class SolutionServiceImpl : AbstractPhoenixService(), SolutionApiService {
     TODO("Not yet implemented")
   }
 
-  override fun upload(
-      organizationId: kotlin.String,
-      body: org.springframework.core.io.Resource
-  ): Solution {
-    TODO("Not yet implemented")
+  override fun upload(organizationId: String, body: org.springframework.core.io.Resource) =
+      body.inputStream.use { inputStream ->
+        val solution =
+            ObjectMapper(YAMLFactory())
+                .registerKotlinModule()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .readValue(inputStream, Solution::class.java)
+        createSolution(organizationId, solution)
+      }
+
+  @EventListener(OrganizationRegistered::class)
+  fun onOrganizationRegistered(organizationRegistered: OrganizationRegistered) {
+    cosmosClient
+        .getDatabase(databaseName)
+        .createContainerIfNotExists(
+            CosmosContainerProperties("${organizationRegistered.organizationId}_solutions", "/id"))
+  }
+
+  @EventListener(OrganizationUnregistered::class)
+  fun onOrganizationUnregistered(organizationUnregistered: OrganizationUnregistered) {
+    cosmosTemplate.deleteContainer("${organizationUnregistered.organizationId}_solutions")
   }
 
   override fun uploadRunTemplateHandler(
