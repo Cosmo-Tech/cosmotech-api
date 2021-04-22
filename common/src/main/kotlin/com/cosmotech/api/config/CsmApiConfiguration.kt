bcbe2852375ff6df8c19d98b85933e54531d689d
Armel Soro <armel.soro@cosmotech.com>
2021-04-22 14:13:10 +0200

    Auto-scan packages for looking up ConfigurationProperties bean
    Rather than hard-coding them.

--- common/src/main/kotlin/com/cosmotech/api/config/CsmApiConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/api/config/CsmApiConfiguration.kt
@@ -2,9 +2,9 @@
 // Licensed under the MIT license.
 package com.cosmotech.api.config
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(CsmPlatformProperties::class)
+@ConfigurationPropertiesScan(basePackages = ["com.cosmotech"])
 class CsmApiConfiguration
