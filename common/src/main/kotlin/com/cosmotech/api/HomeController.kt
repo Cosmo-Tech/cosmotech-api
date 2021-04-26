91526678e242583074bdf9eb58491863d2b8dbef
Armel Soro <armel.soro@cosmotech.com>
2021-04-26 14:57:27 +0200

    Do not include HomeController in the Swagger UI
    Our single source of truth for the endpoints is the OpenAPI definition

--- common/src/main/kotlin/com/cosmotech/api/HomeController.kt
+++ common/src/main/kotlin/com/cosmotech/api/HomeController.kt
@@ -4,10 +4,10 @@ package com.cosmotech.api
 
 import javax.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HomeController {
 
   @Value("\${api.swagger-ui.base-path}") private lateinit var swaggerUiBasePath: String
