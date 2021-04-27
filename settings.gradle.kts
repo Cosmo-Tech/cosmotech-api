e401281aceb506ed72aa443b7b8ab4ceac920de1
Armel Soro <armel.soro@cosmotech.com>
2021-04-27 10:55:34 +0200

    Reorganize settings.gradle.kts to make it legible

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,45 +1,38 @@
 rootProject.name = "cosmotech-api-parent"
 
-include("api")
-
-include("common")
-
-include("organization")
-
-include("user")
-
-include("connector")
-
-include("dataset")
-
-include("solution")
-
-include("workspace")
-
-include("scenario")
-
-include("scenariorun")
-
-include("platform")
+include(
+    "api",
+    "common",
+    "connector",
+    "dataset",
+    "organization",
+    "platform",
+    "scenario",
+    "scenariorun",
+    "solution",
+    "user",
+    // TODO Upgrading to Gradle 7.0 will allow to use Kotlin 1.4 language features, such as trailing
+    // commas, which can be useful here
+    "workspace")
 
 project(":api").name = "cosmotech-api"
 
 project(":common").name = "cosmotech-api-common"
 
-project(":organization").name = "cosmotech-organization-api"
-
-project(":user").name = "cosmotech-user-api"
-
 project(":connector").name = "cosmotech-connector-api"
 
 project(":dataset").name = "cosmotech-dataset-api"
 
-project(":solution").name = "cosmotech-solution-api"
+project(":organization").name = "cosmotech-organization-api"
 
-project(":workspace").name = "cosmotech-workspace-api"
+project(":platform").name = "cosmotech-platform-api"
 
 project(":scenario").name = "cosmotech-scenario-api"
 
 project(":scenariorun").name = "cosmotech-scenariorun-api"
 
-project(":platform").name = "cosmotech-platform-api"
+project(":solution").name = "cosmotech-solution-api"
+
+project(":user").name = "cosmotech-user-api"
+
+project(":workspace").name = "cosmotech-workspace-api"
