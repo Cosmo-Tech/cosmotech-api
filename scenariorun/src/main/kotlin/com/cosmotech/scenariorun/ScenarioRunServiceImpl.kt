b43d758fe51714cf5f5aca65529898808ca4d02d
Armel Soro <armel.soro@cosmotech.com>
2021-04-28 12:31:09 +0200

    [scenariorun] Implement service methods

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ScenarioRunServiceImpl.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ScenarioRunServiceImpl.kt
@@ -2,7 +2,10 @@
 // Licensed under the MIT license.
 package com.cosmotech.scenariorun
 
-import com.cosmotech.api.AbstractPhoenixService
+import com.azure.cosmos.models.*
+import com.cosmotech.api.AbstractCosmosBackedService
+import com.cosmotech.api.utils.convertToMap
+import com.cosmotech.api.utils.toDomain
 import com.cosmotech.scenariorun.api.ScenariorunApiService
 import com.cosmotech.scenariorun.domain.ScenarioRun
 import com.cosmotech.scenariorun.domain.ScenarioRunLogs
@@ -11,97 +14,234 @@ import com.cosmotech.scenariorun.domain.ScenarioRunSearch
 import com.cosmotech.scenariorun.domain.ScenarioRunStart
 import com.cosmotech.scenariorun.domain.ScenarioRunStartContainers
 import com.cosmotech.scenariorun.domain.ScenarioRunStartSolution
+import com.fasterxml.jackson.databind.JsonNode
+import java.util.*
+import kotlin.reflect.full.memberProperties
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
 @Service
-class ScenariorunServiceImpl : AbstractPhoenixService(), ScenariorunApiService {
+@ConditionalOnProperty(name = ["csm.platform.vendor"], havingValue = "azure", matchIfMissing = true)
+class ScenariorunServiceImpl : AbstractCosmosBackedService(), ScenariorunApiService {
 
-  override fun deleteScenarioRun(
-      organizationId: kotlin.String,
-      scenariorunId: kotlin.String
-  ): ScenarioRun {
-    TODO("Not implemented yet")
+  protected fun ScenarioRun.asMapWithAdditionalData(workspaceId: String? = null): Map<String, Any> {
+    val scenarioAsMap = this.convertToMap().toMutableMap()
+    scenarioAsMap["type"] = "ScenarioRun"
+    if (workspaceId != null) {
+      scenarioAsMap["workspaceId"] = workspaceId
+    }
+    return scenarioAsMap
   }
 
-  override fun findScenarioRunById(
-      organizationId: kotlin.String,
-      scenariorunId: kotlin.String
-  ): ScenarioRun {
-    TODO("Not implemented yet")
+  protected fun ScenarioRunSearch.toQueryPredicate(): Pair<String, List<SqlParameter>> {
+    val queryPredicateComponents =
+        this::class
+            .memberProperties
+            .mapNotNull { memberProperty ->
+              val propertyName = memberProperty.name
+              val value: Any? = memberProperty.getter.call(this)
+              if (value == null) null
+              else "c.$propertyName = @$propertyName" to SqlParameter("@$propertyName", value)
+            }
+            .toMap()
+    // TODO Joining with AND or OR ?
+    return queryPredicateComponents.keys.joinToString(separator = " AND ") to
+        queryPredicateComponents.values.toList()
   }
+
+  override fun deleteScenarioRun(organizationId: String, scenariorunId: String): ScenarioRun {
+    val scenarioRun = this.findScenarioRunById(organizationId, scenariorunId)
+    cosmosTemplate.deleteEntity("${organizationId}_scenario_data", scenarioRun)
+    return scenarioRun
+  }
+
+  override fun findScenarioRunById(organizationId: String, scenariorunId: String): ScenarioRun =
+      cosmosCoreDatabase
+          .getContainer("${organizationId}_scenario_data")
+          .queryItems(
+              SqlQuerySpec(
+                  "SELECT * FROM c WHERE c.type = 'ScenarioRun' AND c.id = @SCENARIORUN_ID",
+                  listOf(SqlParameter("@SCENARIORUN_ID", scenariorunId))),
+              CosmosQueryRequestOptions(),
+              // It would be much better to specify the Domain Type right away and
+              // avoid the map operation, but we can't due
+              // to the lack of customization of the Cosmos Client Object Mapper, as reported here :
+              // https://github.com/Azure/azure-sdk-for-java/issues/12269
+              JsonNode::class.java)
+          .firstOrNull()
+          ?.toDomain<ScenarioRun>()
+          ?: throw java.lang.IllegalArgumentException(
+              "ScenarioRun #$scenariorunId not found in organization #$organizationId")
 
   override fun getScenarioScenarioRun(
-      organizationId: kotlin.String,
-      workspaceId: kotlin.String,
-      scenarioId: kotlin.String,
-      scenariorunId: kotlin.String
-  ): ScenarioRun {
-    TODO("Not implemented yet")
-  }
+      organizationId: String,
+      workspaceId: String,
+      scenarioId: String,
+      scenariorunId: String
+  ): ScenarioRun =
+      cosmosCoreDatabase
+          .getContainer("${organizationId}_scenario_data")
+          .queryItems(
+              SqlQuerySpec(
+                  """
+                            SELECT * FROM c 
+                              WHERE c.type = 'ScenarioRun' 
+                                AND c.id = @SCENARIORUN_ID 
+                                AND c.workspaceId = @WORKSPACE_ID 
+                                AND c.scenarioId = @SCENARIO_ID
+                          """.trimIndent(),
+                  listOf(
+                      SqlParameter("@SCENARIORUN_ID", scenariorunId),
+                      SqlParameter("@WORKSPACE_ID", workspaceId),
+                      SqlParameter("@SCENARIO_ID", scenarioId))),
+              CosmosQueryRequestOptions(),
+              // It would be much better to specify the Domain Type right away and
+              // avoid the map operation, but we can't due
+              // to the lack of customization of the Cosmos Client Object Mapper, as reported here :
+              // https://github.com/Azure/azure-sdk-for-java/issues/12269
+              JsonNode::class.java)
+          .firstOrNull()
+          ?.toDomain<ScenarioRun>()
+          ?: throw java.lang.IllegalArgumentException(
+              "ScenarioRun #$scenariorunId not found in organization #$organizationId")
 
   override fun getScenarioScenarioRunLogs(
-      organizationId: kotlin.String,
-      workspaceId: kotlin.String,
-      scenarioId: kotlin.String,
-      scenariorunId: kotlin.String
+      organizationId: String,
+      workspaceId: String,
+      scenarioId: String,
+      scenariorunId: String
   ): ScenarioRunLogs {
     TODO("Not implemented yet")
   }
 
   override fun getScenarioScenarioRuns(
-      organizationId: kotlin.String,
-      workspaceId: kotlin.String,
-      scenarioId: kotlin.String
-  ): List<ScenarioRun> {
-    TODO("Not implemented yet")
-  }
+      organizationId: String,
+      workspaceId: String,
+      scenarioId: String
+  ): List<ScenarioRun> =
+      cosmosCoreDatabase
+          .getContainer("${organizationId}_scenario_data")
+          .queryItems(
+              SqlQuerySpec(
+                  """
+                            SELECT * FROM c 
+                              WHERE c.type = 'ScenarioRun' 
+                                AND c.workspaceId = @WORKSPACE_ID 
+                                AND c.scenarioId = @SCENARIO_ID
+                          """.trimIndent(),
+                  listOf(
+                      SqlParameter("@WORKSPACE_ID", workspaceId),
+                      SqlParameter("@SCENARIO_ID", scenarioId))),
+              CosmosQueryRequestOptions(),
+              // It would be much better to specify the Domain Type right away and
+              // avoid the map operation, but we can't due
+              // to the lack of customization of the Cosmos Client Object Mapper, as reported here :
+              // https://github.com/Azure/azure-sdk-for-java/issues/12269
+              JsonNode::class.java)
+          .mapNotNull { it.toDomain<ScenarioRun>() }
+          .toList()
 
   override fun getWorkspaceScenarioRuns(
-      organizationId: kotlin.String,
-      workspaceId: kotlin.String
-  ): List<ScenarioRun> {
-    TODO("Not implemented yet")
-  }
+      organizationId: String,
+      workspaceId: String
+  ): List<ScenarioRun> =
+      cosmosCoreDatabase
+          .getContainer("${organizationId}_scenario_data")
+          .queryItems(
+              SqlQuerySpec(
+                  """
+                            SELECT * FROM c 
+                              WHERE c.type = 'ScenarioRun' 
+                                AND c.workspaceId = @WORKSPACE_ID
+                          """.trimIndent(),
+                  listOf(SqlParameter("@WORKSPACE_ID", workspaceId))),
+              CosmosQueryRequestOptions(),
+              // It would be much better to specify the Domain Type right away and
+              // avoid the map operation, but we can't due
+              // to the lack of customization of the Cosmos Client Object Mapper, as reported here :
+              // https://github.com/Azure/azure-sdk-for-java/issues/12269
+              JsonNode::class.java)
+          .mapNotNull { it.toDomain<ScenarioRun>() }
+          .toList()
 
   override fun runScenario(
-      organizationId: kotlin.String,
-      workspaceId: kotlin.String,
-      scenarioId: kotlin.String
+      organizationId: String,
+      workspaceId: String,
+      scenarioId: String
   ): ScenarioRun {
+
+    // TODO Implement logic to submit Kubernetes Job
+
+    val scenarioRun =
+        ScenarioRun(
+            id = UUID.randomUUID().toString(),
+            scenarioId = scenarioId,
+            workspaceId = workspaceId,
+            // TODO Set other parameters here
+            )
+
+    val scenarioRunAsMap = scenarioRun.asMapWithAdditionalData(workspaceId)
+    // We cannot use cosmosTemplate as it expects the Domain object to contain a field named 'id'
+    // or annotated with @Id
+    if (cosmosCoreDatabase
+        .getContainer("${organizationId}_scenario_data")
+        .createItem(scenarioRunAsMap, PartitionKey(scenarioRun.ownerId), CosmosItemRequestOptions())
+        .item == null) {
+      throw IllegalArgumentException("No ScenarioRun returned in response: $scenarioRunAsMap")
+    }
+
     TODO("Not implemented yet")
   }
 
   override fun searchScenarioRunLogs(
-      organizationId: kotlin.String,
-      scenariorunId: kotlin.String,
+      organizationId: String,
+      scenariorunId: String,
       scenarioRunLogsOptions: ScenarioRunLogsOptions
   ): ScenarioRunLogs {
     TODO("Not implemented yet")
   }
 
   override fun searchScenarioRuns(
-      organizationId: kotlin.String,
+      organizationId: String,
       scenarioRunSearch: ScenarioRunSearch
   ): List<ScenarioRun> {
-    TODO("Not implemented yet")
+    val scenarioRunSearchPredicatePair = scenarioRunSearch.toQueryPredicate()
+    return cosmosCoreDatabase
+        .getContainer("${organizationId}_scenario_data")
+        .queryItems(
+            SqlQuerySpec(
+                """
+                            SELECT * FROM c 
+                              WHERE c.type = 'ScenarioRun' 
+                              ${if (scenarioRunSearchPredicatePair.first.isNotBlank()) " AND ( ${scenarioRunSearchPredicatePair.first} )" else ""}
+                          """.trimIndent(),
+                scenarioRunSearchPredicatePair.second),
+            CosmosQueryRequestOptions(),
+            // It would be much better to specify the Domain Type right away and
+            // avoid the map operation, but we can't due
+            // to the lack of customization of the Cosmos Client Object Mapper, as reported here :
+            // https://github.com/Azure/azure-sdk-for-java/issues/12269
+            JsonNode::class.java)
+        .mapNotNull { it.toDomain<ScenarioRun>() }
+        .toList()
   }
 
   override fun startScenarioRunContainers(
-      organizationId: kotlin.String,
+      organizationId: String,
       scenarioRunStartContainers: ScenarioRunStartContainers
   ): ScenarioRun {
     TODO("Not implemented yet")
   }
 
   override fun startScenarioRunScenario(
-      organizationId: kotlin.String,
+      organizationId: String,
       scenarioRunStart: ScenarioRunStart
   ): ScenarioRun {
     TODO("Not implemented yet")
   }
 
   override fun startScenarioRunSolution(
-      organizationId: kotlin.String,
+      organizationId: String,
       scenarioRunStartSolution: ScenarioRunStartSolution
   ): ScenarioRun {
     TODO("Not implemented yet")
