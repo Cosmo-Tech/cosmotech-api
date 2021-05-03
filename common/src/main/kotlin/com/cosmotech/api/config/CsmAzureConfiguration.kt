3cd11c77ac040b1b4489bca6ed59adb949ba4808
Armel Soro <armel.soro@cosmotech.com>
2021-05-03 15:36:38 +0200

    refactor: Move CsmAzureCosmosHealthConfiguration to CsmAzureConfiguration.kt

--- common/src/main/kotlin/com/cosmotech/api/config/CsmAzureConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/api/config/CsmAzureConfiguration.kt
@@ -2,15 +2,37 @@
 // Licensed under the MIT license.
 package com.cosmotech.api.config
 
+import com.azure.cosmos.CosmosAsyncClient
 import com.azure.cosmos.CosmosClient
 import com.azure.cosmos.CosmosClientBuilder
+import com.azure.spring.autoconfigure.cosmos.CosmosHealthConfiguration
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 @ConditionalOnProperty(name = ["csm.platform.vendor"], havingValue = "azure", matchIfMissing = true)
 class CsmAzureConfiguration(val cosmosClientBuilder: CosmosClientBuilder) {
 
   @Bean fun cosmosClient(): CosmosClient = cosmosClientBuilder.buildClient()
+}
+
+/**
+ * The intent of this configuration class is to enable the Azure Cosmos Health Indicator, based on
+ * our own 'csm.platform.vendor' property value, irrespective of the value of
+ * 'management.health.azure-cosmos.enabled'.
+ *
+ * We actually should always enable this indicator as soon as 'csm.platform.vendor' is set to
+ * 'azure'
+ */
+@Configuration
+@ConditionalOnProperty(name = ["csm.platform.vendor"], havingValue = "azure", matchIfMissing = true)
+class CsmAzureCosmosHealthConfiguration : CosmosHealthConfiguration() {
+
+  @Bean
+  @Primary
+  override fun cosmosHealthContributor(cosmosAsyncClient: CosmosAsyncClient): HealthIndicator =
+      super.cosmosHealthContributor(cosmosAsyncClient)
 }
