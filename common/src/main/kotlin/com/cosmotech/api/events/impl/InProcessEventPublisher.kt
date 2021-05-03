7422ee7539c7bc2ece35c9257d2b2a57ead64d8a
Armel Soro <armel.soro@cosmotech.com>
2021-05-03 11:12:15 +0200

    Autowire InProcessEventPublisher#eventPublisher via constructor injection and reduce its visibility

--- common/src/main/kotlin/com/cosmotech/api/events/impl/InProcessEventPublisher.kt
+++ common/src/main/kotlin/com/cosmotech/api/events/impl/InProcessEventPublisher.kt
@@ -4,7 +4,6 @@ package com.cosmotech.api.events.impl
 
 import com.cosmotech.api.events.CsmEvent
 import com.cosmotech.api.events.CsmEventPublisher
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
@@ -12,9 +11,8 @@ import org.springframework.stereotype.Service
 @Service
 @ConditionalOnProperty(
     name = ["csm.platform.event-publisher.type"], havingValue = "in_process", matchIfMissing = true)
-class InProcessEventPublisher : CsmEventPublisher {
-
-  @Autowired protected lateinit var eventPublisher: ApplicationEventPublisher
+class InProcessEventPublisher(private val eventPublisher: ApplicationEventPublisher) :
+    CsmEventPublisher {
 
   override fun <T : CsmEvent> publishEvent(event: T) {
     this.eventPublisher.publishEvent(event)
