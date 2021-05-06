641a3e300e930b8a711b6d077ab3c2ac746f90cf
Armel Soro <armel.soro@cosmotech.com>
2021-05-06 14:56:30 +0200

    chore(deps): Bump Kotlin to 1.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-  val kotlinVersion = "1.4.32"
+  val kotlinVersion = "1.5.0"
   kotlin("jvm") version kotlinVersion
   kotlin("kapt") version kotlinVersion
   kotlin("plugin.spring") version kotlinVersion apply false
@@ -123,7 +123,7 @@ subprojects {
 
   tasks.withType<KotlinCompile> {
     kotlinOptions {
-      languageVersion = "1.4"
+      languageVersion = "1.5"
       freeCompilerArgs = listOf("-Xjsr305=strict")
       jvmTarget = "11"
     }
