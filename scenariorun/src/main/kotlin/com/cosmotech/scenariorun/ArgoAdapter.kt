6ef26626e8a7d93efbd1fb26dda25be0c00bbd7c
Vincent Carluer <vcarluer@gmail.com>
2021-05-06 17:29:04 +0200

    test in progress

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ArgoAdapter.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ArgoAdapter.kt
@@ -3,33 +3,71 @@
 package com.cosmotech.scenariorun
 
 import com.cosmotech.scenariorun.domain.ScenarioRunContainer
+import com.cosmotech.scenariorun.domain.ScenarioRunStartContainers
 import io.argoproj.workflow.models.Template
+import io.argoproj.workflow.models.WorkflowSpec
 import io.kubernetes.client.openapi.models.V1EnvVar
 import org.slf4j.LoggerFactory
 
 class ArgoAdapter {
   private val logger = LoggerFactory.getLogger(ArgoAdapter::class.java)
+  private val K8S_AGENT_POOL = "agentpool"
+  private val CSM_DEFAULT_POOL = "basicpool"
+  private val CSM_DEFAULT_ACCOUNT = "workflow"
+  private val CSM_DAG_ENTRYPOINT = "entrypoint"
 
   fun buildTemplate(
-      scenarioRunContainer: ScenarioRunContainer,
-      name: String = "default"
+      scenarioRunContainer: ScenarioRunContainer
   ): Template {
     var envVars: MutableList<V1EnvVar>? = null
     if (scenarioRunContainer.envVars != null) {
       envVars = mutableListOf()
       scenarioRunContainer.envVars.forEach { (key, value) ->
-        val envVar = V1EnvVar().name(key).value(value.toString())
+        val envVar = V1EnvVar().name(key).value(value)
         envVars.add(envVar)
       }
     }
 
     return Template()
-        .name(name)
+        .name(scenarioRunContainer.name)
         .container(
             io.kubernetes.client.openapi.models.V1Container()
                 .image(scenarioRunContainer.image)
                 .command(listOf(scenarioRunContainer.entrypoint))
                 .env(envVars)
                 .args(scenarioRunContainer.runArgs))
+  }
+
+  fun buildWorkflowSpec(startContainers: ScenarioRunStartContainers): WorkflowSpec {
+    val nodeSelector = buildNodeSelector(startContainers)
+    val templates = buildContainersTemplates(startContainers)
+    val entrypointTemplate = buildEntrypointTemplate(startContainers)
+    templates.add(entrypointTemplate)
+
+    return WorkflowSpec()
+        .nodeSelector(nodeSelector)
+        .serviceAccountName(CSM_DEFAULT_ACCOUNT)
+        .entrypoint(CSM_DAG_ENTRYPOINT)
+        .templates(templates)
+  }
+
+  private fun buildNodeSelector(startContainers: ScenarioRunStartContainers): Map<String, String> {
+    val nodeSelector = mutableMapOf("kubernetes.io/os" to "linux")
+
+    if (startContainers.nodeLabel != null)
+        nodeSelector.put(K8S_AGENT_POOL, startContainers.nodeLabel)
+    else nodeSelector.put(K8S_AGENT_POOL, CSM_DEFAULT_POOL)
+
+    return nodeSelector
+  }
+
+  private fun buildContainersTemplates(startContainers: ScenarioRunStartContainers): MutableList<Template> {
+    val list = startContainers.containers.map {
+      container -> buildTemplate(container)
+    }
+    return list.toMutableList()
+  }
+  private fun buildEntrypointTemplate(startContainers: ScenarioRunStartContainers): Template {
+    return Template().name(CSM_DAG_ENTRYPOINT)
   }
 }
--- scenariorun/src/test/kotlin/com/cosmotech/scenariorun/ArgoAdapterTests.kt
+++ scenariorun/src/test/kotlin/com/cosmotech/scenariorun/ArgoAdapterTests.kt
@@ -3,6 +3,7 @@
 package com.cosmotech.scenariorun
 
 import com.cosmotech.scenariorun.domain.ScenarioRunContainer
+import com.cosmotech.scenariorun.domain.ScenarioRunStartContainers
 import io.kubernetes.client.openapi.models.V1EnvVar
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -29,9 +30,9 @@ class ArgoAdapterTests {
 
   @Test
   fun `Template has name`() {
-    val src = getScenarioRunContainer()
     val name = "template name"
-    val template = argoAdapter.buildTemplate(src, name)
+    val src = getScenarioRunContainer(name)
+    val template = argoAdapter.buildTemplate(src)
     assertEquals(name, template.name)
   }
 
@@ -85,33 +86,130 @@ class ArgoAdapterTests {
     assertEquals(expected, template.container?.env)
   }
 
-  fun getScenarioRunContainer(): ScenarioRunContainer {
+  @Test
+  fun `Create Workflow Spec with StartContainers not null`() {
+    var sc = getStartContainersRun()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+    assertNotNull(workflowSpec)
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers agent pool`() {
+    var sc = getStartContainersRun()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+    val expected = mapOf("kubernetes.io/os" to "linux", "agentpool" to "highcpupool")
+
+    assertTrue(expected.equals(workflowSpec.nodeSelector))
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers default agent pool`() {
+    var sc = getStartContainersRunDefaultPool()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+    val expected = mapOf("kubernetes.io/os" to "linux", "agentpool" to "basicpool")
+
+    assertTrue(expected.equals(workflowSpec.nodeSelector))
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers Service Account`() {
+    var sc = getStartContainersRun()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+
+    assertEquals("workflow", workflowSpec.serviceAccountName)
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers Run name`() {
+    var sc = getStartContainersRun()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+
+    assertEquals("runContainer", workflowSpec.templates?.getOrNull(0)?.name)
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers Entrypoint FetchDataset`() {
+    var sc = getStartContainers()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+
+    assertEquals("entrypoint", workflowSpec.entrypoint)
+  }
+
+  @Test
+  fun `Create Workflow Spec with StartContainers entrypoint step not null`() {
+    var sc = getStartContainers()
+    val workflowSpec = argoAdapter.buildWorkflowSpec(sc)
+
+    val entrypointTemplate = workflowSpec.templates?.find {
+      template -> template.name.equals("entrypoint")
+    }
+    assertNotNull(entrypointTemplate)
+  }
+
+  fun getScenarioRunContainer(name: String = "default"): ScenarioRunContainer {
     var src =
         ScenarioRunContainer(
+           name = name, 
             image = "cosmotech/testcontainer",
         )
     return src
   }
 
-  fun getScenarioRunContainerArgs(): ScenarioRunContainer {
+  fun getScenarioRunContainerArgs(name: String = "default"): ScenarioRunContainer {
     var src =
         ScenarioRunContainer(
+           name = name, 
             image = "cosmotech/testcontainer", runArgs = listOf("arg1", "arg2", "arg3"))
     return src
   }
 
-  fun getScenarioRunContainerEntrypoint(): ScenarioRunContainer {
+  fun getScenarioRunContainerEntrypoint(name: String = "default"): ScenarioRunContainer {
     var src =
-        ScenarioRunContainer(image = "cosmotech/testcontainer", entrypoint = DEFAULT_ENTRY_POINT)
+        ScenarioRunContainer(
+           name = name, 
+          image = "cosmotech/testcontainer", entrypoint = DEFAULT_ENTRY_POINT)
     return src
   }
 
-  fun getScenarioRunContainerEnv(): ScenarioRunContainer {
+  fun getScenarioRunContainerEnv(name: String = "default"): ScenarioRunContainer {
     var src =
         ScenarioRunContainer(
+           name = name, 
             image = "cosmotech/testcontainer",
             envVars = mapOf("env1" to "envvar1", "env2" to "envvar2", "env3" to "envvar3"))
 
     return src
   }
+
+  fun getStartContainersRunDefaultPool(): ScenarioRunStartContainers {
+    val sc = ScenarioRunStartContainers(containers = listOf(getScenarioRunContainerEntrypoint()))
+    return sc
+  }
+
+  fun getStartContainersRun(): ScenarioRunStartContainers {
+    val sc =
+        ScenarioRunStartContainers(
+            nodeLabel = "highcpupool",
+            containers = listOf(getScenarioRunContainerEntrypoint("runContainer")))
+    return sc
+  }
+
+  fun getStartContainers(): ScenarioRunStartContainers {
+    val sc =
+        ScenarioRunStartContainers(
+            nodeLabel = "highcpupool",
+            containers = listOf(
+            getScenarioRunContainer("fetchDatasetContainer-1"),
+            getScenarioRunContainer("fetchScenarioParametersContainer"),
+            getScenarioRunContainerEntrypoint("applyParametersContainer"),
+            getScenarioRunContainerEntrypoint("validateDataContainer"),
+            getScenarioRunContainer("sendDataWarehouseContainer"),
+            getScenarioRunContainerEntrypoint("preRunContainer"),
+            getScenarioRunContainerEntrypoint("runContainer"),
+            getScenarioRunContainerEntrypoint("postRunContainer"),
+          )
+        )
+    return sc
+  }
+
 }
