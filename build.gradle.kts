c5e639d071d095130b2416460066a1a3ca32263a
Armel Soro <armel.soro@cosmotech.com>
2021-05-07 14:48:04 +0200

    Remove Spring Boot Configuration annotation processor
    Addendum to f7590ba

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,8 +109,6 @@ subprojects {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("javax.validation:validation-api:2.0.1.Final")
 
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
     implementation("org.springframework.cloud:spring-cloud-starter-kubernetes-fabric8-config:2.0.2")
 
     val springDocVersion = "1.5.8"
