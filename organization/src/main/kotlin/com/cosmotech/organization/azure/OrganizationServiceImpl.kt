a93cbb7123a38f4ba5a6d8d053cea29e9588fd64
Armel Soro <armel.soro@cosmotech.com>
2021-05-07 16:04:18 +0200

    [organization] Move Service class to a dedicated 'com.cosmotech.connector.azure' package
    This implementation is specific to Azure

--- organization/src/main/kotlin/com/cosmotech/organization/azure/OrganizationServiceImpl.kt
+++ organization/src/main/kotlin/com/cosmotech/organization/azure/OrganizationServiceImpl.kt
@@ -1,6 +1,6 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-package com.cosmotech.organization
+package com.cosmotech.organization.azure
 
 import com.azure.cosmos.models.CosmosContainerProperties
 import com.cosmotech.api.AbstractCosmosBackedService
