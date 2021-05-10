737bd7f7dbf914080e963e83c83e32140325afa5
Armel Soro <armel.soro@cosmotech.com>
2021-05-10 14:04:29 +0200

    [scenario] Move Service class to a dedicated 'com.cosmotech.scenario.azure' package
    This implementation is specific to Azure

--- scenario/src/main/kotlin/com/cosmotech/scenario/azure/ScenarioServiceImpl.kt
+++ scenario/src/main/kotlin/com/cosmotech/scenario/azure/ScenarioServiceImpl.kt
@@ -1,6 +1,6 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
-package com.cosmotech.scenario
+package com.cosmotech.scenario.azure
 
 import com.azure.cosmos.models.*
 import com.cosmotech.api.AbstractCosmosBackedService
