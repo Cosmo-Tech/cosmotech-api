60555502a79c60deaad5fd3c6e971f05604a076b
Armel Soro <armel.soro@cosmotech.com>
2021-05-21 11:03:17 +0200

    [helm] Add sample Helper script to deploy both Argo and cosmotech-api Charts
    They are deployed as separate Helm releases

---
+++ api/kubernetes/deploy_via_helm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo errexit
+
+help() {
+  echo "This script takes at least 3 parameters."
+  echo "Usage: ./$(basename "$0") NAMESPACE ARGO_POSTGRESQL_PASSWORD API_VERSION [any additional options to pass as is to the cosmotech-api Helm Chart]"
+}
+
+if [[ "${1:-}" == "--help" ||  "${1:-}" == "-h" ]]; then
+  help
+  exit 0
+fi
+if [[ $# -lt 3 ]]; then
+  help
+  exit 1
+fi
+
+export ARGO_RELEASE_NAME=argo
+
+export NAMESPACE="$1"
+export ARGO_POSTGRESQL_PASSWORD="$2"
+export API_VERSION="$3"
+
+export COSMOTECH_API_RELEASE_NAME="cosmotech-api-${API_VERSION}"
+
+HELM_CHARTS_BASE_PATH=$(realpath "$(dirname "$0")/../../api/kubernetes/")
+
+# Create namespace if it does not exist
+kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
+
+# Argo
+helm dependency update "${HELM_CHARTS_BASE_PATH}/csm-argo"
+envsubst < "${HELM_CHARTS_BASE_PATH}/csm-argo/values.yaml" | \
+    helm upgrade --install "${ARGO_RELEASE_NAME}" "${HELM_CHARTS_BASE_PATH}/csm-argo" \
+        --namespace "${NAMESPACE}" \
+        --values -
+
+# cosmotech-api
+helm upgrade --install "${COSMOTECH_API_RELEASE_NAME}" "${HELM_CHARTS_BASE_PATH}/helm-chart" \
+    --namespace "${NAMESPACE}" \
+    "${@:4}" \
+    --set api.version="$API_VERSION" \
+    --set config.csm.platform.argo.base-url="http://${ARGO_RELEASE_NAME}-server.${NAMESPACE}.svc.cluster.local:2746"
