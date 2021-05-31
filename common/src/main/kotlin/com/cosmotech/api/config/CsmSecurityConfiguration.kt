8f06ccd5e688f23eaa4cbccc5c79ec788bbf5bf2
Vincent Carluer <vcarluer@gmail.com>
2021-05-31 21:15:27 +0200

    reader & writer roles list and utilitary functions

--- common/src/main/kotlin/com/cosmotech/api/config/CsmSecurityConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/api/config/CsmSecurityConfiguration.kt
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
@@ -22,6 +23,67 @@ import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.jwt.JwtClaimValidator
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+
+// Business roles
+const val ROLE_PLATFORM_ADMIN = "APPROLE_Platform.Admin"
+const val ROLE_CONNECTOR_DEVELOPER = "APPROLE_Connector.Developer"
+const val ROLE_ORGANIZATION_ADMIN = "APPROLE_Organization.Admin"
+const val ROLE_ORGANIZATION_COLLABORATOR = "APPROLE_Organization.Collaborator"
+const val ROLE_ORGANIZATION_MODELER = "APPROLE_Organization.Modeler"
+const val ROLE_ORGANIZATION_USER = "APPROLE_Organization.User"
+const val ROLE_ORGANIZATION_VIEWER = "APPROLE_Organization.Viewer"
+
+// Endpoints roles
+const val ROLE_CONNECTOR_READER = "APPROLE_Connector.Reader"
+const val ROLE_CONNECTOR_WRITER = "APPROLE_Connector.Writer"
+const val ROLE_DATASET_READER = "APPROLE_Dataset.Reader"
+const val ROLE_DATASET_WRITER = "APPROLE_Dataset.Writer"
+const val ROLE_ORGANIZATION_READER = "APPROLE_Organization.Reader"
+const val ROLE_ORGANIZATION_WRITER = "APPROLE_Organization.Writer"
+const val ROLE_SCENARIO_READER = "APPROLE_Scenario.Reader"
+const val ROLE_SCENARIO_WRITER = "APPROLE_Scenario.Writer"
+const val ROLE_SCENARIORUN_READER = "APPROLE_ScenarioRun.Reader"
+const val ROLE_SCENARIORUN_WRITER = "APPROLE_ScenarioRun.Writer"
+const val ROLE_SOLUTION_READER = "APPROLE_Solution.Reader"
+const val ROLE_SOLUTION_WRITER = "APPROLE_Solution.Writer"
+const val ROLE_WORKSPACE_READER = "APPROLE_Workspace.Reader"
+const val ROLE_WORKSPACE_WRITER = "APPROLE_Workspace.Writer"
+
+// Endpoints paths
+const val PATH_CONNECTORS = "/connectors"
+const val PATH_ORGANIZATIONS = "/organizations"
+
+// Endpoints roles
+val endpointSecurityPublic =
+    listOf(
+        "/actuator/health/**",
+        "/actuator/info",
+        "/",
+        "/swagger-ui.html",
+        "/swagger-ui/**",
+        "/openapi.*",
+        "/openapi/*",
+        "/openapi",
+        "/error",
+    )
+val endpointSecurityReaders =
+    mapOf(
+        PATH_CONNECTORS to
+            arrayOf(
+                ROLE_CONNECTOR_READER,
+                ROLE_CONNECTOR_WRITER,
+                ROLE_CONNECTOR_DEVELOPER,
+                ROLE_ORGANIZATION_ADMIN,
+                ROLE_ORGANIZATION_COLLABORATOR,
+                ROLE_ORGANIZATION_MODELER,
+                ROLE_ORGANIZATION_USER,
+                ROLE_ORGANIZATION_VIEWER),
+    )
+
+val endpointSecurityWriters =
+    mapOf(
+        PATH_CONNECTORS to arrayOf(ROLE_CONNECTOR_WRITER, ROLE_CONNECTOR_DEVELOPER),
+    )
 
 @Configuration
 @EnableWebSecurity
@@ -37,31 +99,24 @@ class CsmSecurityConfiguration(
   override fun configure(http: HttpSecurity) {
     http
         .authorizeRequests { requests ->
-          requests
-              .antMatchers(HttpMethod.OPTIONS, "/**")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/actuator/health/**")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/actuator/info")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/swagger-ui.html")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/swagger-ui/**")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/openapi.*")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/openapi/*")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/openapi")
-              .permitAll()
-              .antMatchers(HttpMethod.GET, "/error")
-              .permitAll()
-              .antMatchers("/connectors", "/connectors/**")
-              .hasAuthority("APPROLE_Platform.Admin")
-              .anyRequest()
-              .authenticated()
+          requests.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+          // Public paths
+          endpointSecurityPublic.forEach { path ->
+            requests.antMatchers(HttpMethod.GET, path).permitAll()
+          }
+
+          // Endpoint security for reader roles
+          endpointSecurityReaders.forEach { (path, roles) ->
+            this.addReaderRoles(requests, path, *roles)
+          }
+
+          // Endpoint security for writer roles
+          endpointSecurityWriters.forEach { (path, roles) ->
+            this.addWriterRoles(requests, path, *roles)
+          }
+
+          requests.anyRequest().authenticated()
         }
         .oauth2ResourceServer()
         .jwt()
@@ -69,6 +124,32 @@ class CsmSecurityConfiguration(
             AADJwtBearerTokenAuthenticationConverter().apply {
               setPrincipalClaimName(csmPlatformProperties.authorization.principalJwtClaim)
             })
+  }
+
+  private fun addReaderRoles(
+      requests: ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry,
+      path: String,
+      vararg roles: String
+  ) {
+    requests
+        .antMatchers(HttpMethod.GET, path, "${path}/*")
+        .hasAnyAuthority(ROLE_PLATFORM_ADMIN, *roles)
+  }
+
+  private fun addWriterRoles(
+      requests: ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry,
+      path: String,
+      vararg roles: String
+  ) {
+    requests
+        .antMatchers(HttpMethod.POST, path, "${path}/*")
+        .hasAnyAuthority(ROLE_PLATFORM_ADMIN, *roles)
+    requests
+        .antMatchers(HttpMethod.PATCH, path, "${path}/*")
+        .hasAnyAuthority(ROLE_PLATFORM_ADMIN, *roles)
+    requests
+        .antMatchers(HttpMethod.DELETE, path, "${path}/*")
+        .hasAnyAuthority(ROLE_PLATFORM_ADMIN, *roles)
   }
 
   @Bean
