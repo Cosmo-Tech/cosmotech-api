eb1ebf33eafa4a4e025e91dd1ea20dc03dc5b702
Armel Soro <armel.soro@cosmotech.com>
2021-09-20 17:55:24 +0200

    chore(deps): Bump Kotlin to 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-  val kotlinVersion = "1.5.30"
+  val kotlinVersion = "1.5.31"
   kotlin("jvm") version kotlinVersion
   kotlin("plugin.spring") version kotlinVersion apply false
 
