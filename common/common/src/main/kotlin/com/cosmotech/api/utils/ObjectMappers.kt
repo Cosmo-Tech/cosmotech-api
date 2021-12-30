f7cac32f228f584962a687ec39ae7f8682bb870b
Armel Soro <armel.soro@cosmotech.com>
2021-12-30 11:30:34 +0100

    Refactor extension classes in the common module

--- common/common/src/main/kotlin/com/cosmotech/api/utils/ObjectMappers.kt
+++ common/common/src/main/kotlin/com/cosmotech/api/utils/ObjectMappers.kt
@@ -4,6 +4,7 @@ package com.cosmotech.api.utils
 
 import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
@@ -18,3 +19,17 @@ fun objectMapper(jsonFactory: JsonFactory? = null): ObjectMapper =
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
 fun yamlObjectMapper(): ObjectMapper = objectMapper(YAMLFactory())
+
+/**
+ * Convert any JsonNode as an object of the domain type specified.
+ *
+ * This is a workaround due to Azure Cosmos SDK not being able to deserialize Kotlin data classes
+ * [1].
+ *
+ * @see [1] https://github.com/Azure/azure-sdk-for-java/issues/12269
+ */
+inline fun <reified T> JsonNode?.toDomain(objectMapper: ObjectMapper? = null): T? =
+    if (this == null) null
+    else {
+      (objectMapper ?: objectMapper()).treeToValue(this, T::class.java)
+    }
