8ca62eb8da7fea19b91207cd2ad6737f25ff35b9
Armel Soro <armel.soro@cosmotech.com>
2022-01-05 17:19:03 +0100

    [PROD-7420][PROD-8148] Introduce an Azure Data Explorer Client
    This would serve as a base for #154 and #186
    
    [1] https://github.com/Cosmo-Tech/cosmotech-api/pull/154
    [2] https://github.com/Cosmo-Tech/cosmotech-api/pull/186

--- common/azure/build.gradle.kts
+++ common/azure/build.gradle.kts
@@ -13,4 +13,11 @@ dependencies {
   implementation("com.azure.spring:azure-spring-boot-starter-storage")
   api("com.azure:azure-storage-blob-batch")
   implementation("com.azure.spring:azure-spring-boot-starter-active-directory")
+  implementation("com.microsoft.azure.kusto:kusto-ingest:2.8.2") {
+    exclude(group = "org.slf4j", module = "slf4j-api")
+    because(
+        "this depends on org.slf4j:slf4j-api 1.8.0-beta4 (pre 2.x)," +
+            "which is not backward-compatible with 1.7.x." +
+            "See http://www.slf4j.org/faq.html#changesInVersion200")
+  }
 }
