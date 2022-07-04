df16edc8d837b0bc8e4a8c514889d2315a643ec2
jreynard <jeremy.reynard@cosmotech.com>
2022-07-04 15:33:02 +0200

    Add remove_cronjob script

---
+++ api/kubernetes/remove_cronjob.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eo errexit
+
+#
+# Dev version for deploying the Helm Charts using the local Charts folders
+#
+
+help() {
+  echo
+  echo "This script takes at least 5 parameters."
+  echo
+  echo "The following optional environment variables can be set to alter this script behavior:"
+  echo "- NAMESPACE | string | name of the targeted namespace. Generated when not set"
+  echo "- CRONJOB_NAME | string | ex: my-beautiful-cron-job"
+  echo
+  echo "Usage: ./$(basename "$0") NAMESPACE CRONJOB_NAME"
+}
+
+if [[ "${1:-}" == "--help" ||  "${1:-}" == "-h" ]]; then
+  help
+  exit 0
+fi
+if [[ $# -lt 2 ]]; then
+  help
+  exit 1
+fi
+
+kubectl delete -n "${NAMESPACE}" "${CRONJOB_NAME}"
