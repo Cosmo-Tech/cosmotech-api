bc75ad026dd1ae796a1d0bd83466370c7db18adb
Sergey Polyarus <spolyarus@L2206-00536.Cosmo>
2022-10-14 12:42:52 +0200

    Patch endpoint for AccessControl

--- organization/build.gradle.kts
+++ organization/build.gradle.kts
@@ -1,4 +1,8 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-dependencies { implementation(projects.cosmotechUserApi) }
+dependencies {
+  implementation(projects.cosmotechUserApi)
+  testImplementation("org.testng:testng:7.1.0")
+  testImplementation("org.testng:testng:7.1.0")
+}
