5d5cfde2c26e9cec67bd72ddedf523c307c9c388
Michel Guillon <michel.guillon@cosmotech.com>
2022-10-31 15:25:38 +0100

    Changes based on code review

--- scenario/src/main/kotlin/com/cosmotech/scenario/azure/ScenarioServiceImpl.kt
+++ scenario/src/main/kotlin/com/cosmotech/scenario/azure/ScenarioServiceImpl.kt
@@ -227,7 +227,7 @@ internal class ScenarioServiceImpl(
     return scenarioToSave
   }
 
-  @Suppress("NestedBlockDepth", "UnusedPrivateMember")
+  @Suppress("NestedBlockDepth")
   private fun handleScenarioRunTemplateParametersValues(
       parentId: String?,
       solution: Solution?,
@@ -843,7 +843,9 @@ internal class ScenarioServiceImpl(
 
     when (eventBus.authentication.strategy) {
       SHARED_ACCESS_POLICY -> {
-        val (name, key) = kubernetesClient.getSecretFromKubernetes(eventHubNamespace, "phoenix")
+        val (name, key) =
+            kubernetesClient.getSecretFromKubernetes(
+                eventHubNamespace, csmPlatformProperties.namespace)
 
         azureEventHubsClient.sendMetaData(
             baseHostName,
