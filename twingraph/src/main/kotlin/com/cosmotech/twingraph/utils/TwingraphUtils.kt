6d240dde4d8f2df8a23a38ede2a2979b3b81b87a
jreynard <jeremy.reynard@cosmotech.com>
2022-11-24 16:07:14 +0100

    Restrict some keywords in possible query

---
+++ twingraph/src/main/kotlin/com/cosmotech/twingraph/utils/TwingraphUtils.kt
@@ -0,0 +1,26 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.twingraph.utils
+
+import com.cosmotech.api.utils.objectMapper
+import redis.clients.jedis.graph.entities.Node
+import redis.clients.jedis.graph.entities.Property
+
+object TwingraphUtils {
+
+  @JvmStatic
+  fun getNodeJson(node: Node): String {
+    val graphNode =
+        GraphNode(node.getLabel(0), node.id, node.entityPropertyNames.map { node.getProperty(it) })
+    return objectMapper().writeValueAsString(graphNode)
+  }
+  @JvmStatic
+  fun checkReadOnlyQuery(query: String): Boolean {
+    val queryNormalized = query.trim().lowercase()
+    val matchResults =
+        "\\b(create|set|merge|delete|remove)\\b".toRegex().findAll(queryNormalized).toList()
+    return matchResults.isEmpty()
+  }
+}
+
+data class GraphNode(var label: String, var id: Long, var properties: List<Property<Any>>)
