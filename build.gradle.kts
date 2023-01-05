2e9db129d424785811030ab4b121227f3402dd7e
jreynard <jeremy.reynard@cosmotech.com>
2023-01-05 17:08:10 +0100

    exclude slf4j-api from cosmotech-api-common because dependencies incompatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -222,7 +222,14 @@ subprojects {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    api("com.github.Cosmo-Tech:cosmotech-api-common:$cosmotechApiCommonVersion")
+    api("com.github.Cosmo-Tech:cosmotech-api-common:$cosmotechApiCommonVersion") {
+      exclude(group = "org.slf4j", module = "slf4j-api")
+      because(
+          "this depends on org.slf4j:slf4j-api 2.0.3," +
+              "which is not backward-compatible with 1.7.x." +
+              "See http://www.slf4j.org/faq.html#changesInVersion200")
+    }
+
     api("com.github.Cosmo-Tech:cosmotech-api-azure:$cosmotechApiAzureVersion") {
       exclude(group = "org.slf4j", module = "slf4j-api")
       because(
