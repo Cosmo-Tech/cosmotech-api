7d2548fedeb7bb48702cfedfe3a092035ed0e2e1
jreynard <jeremy.reynard@cosmotech.com>
2023-01-10 16:08:10 +0100

    Fix
    Add missing properties in application-test.yml
    Upgrade redis-om-spring version to 0.6.3
    Change annotation @EnableRedisRepositories to @EnableRedisDocumentRepositories

--- api/src/main/kotlin/com/cosmotech/api/CsmApiApplication.kt
+++ api/src/main/kotlin/com/cosmotech/api/CsmApiApplication.kt
@@ -10,8 +10,9 @@ import org.springframework.context.annotation.FilterType
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableRedisDocumentRepositories(basePackages = ["com.cosmotech"])
-@EnableScheduling
+@EnableRedisDocumentRepositories(
+    basePackages = ["com.cosmotech"]
+)@EnableScheduling
 @ComponentScan(
     basePackages = ["com.cosmotech"],
     excludeFilters =
