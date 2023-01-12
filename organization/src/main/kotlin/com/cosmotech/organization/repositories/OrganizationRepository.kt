a8cc018d52284311828cc8755482975fb44c045d
icreated <sergey.polyarus.ext@cosmotech.com>
2023-01-12 16:33:35 +0100

    Detekt

--- organization/src/main/kotlin/com/cosmotech/organization/repositories/OrganizationRepository.kt
+++ organization/src/main/kotlin/com/cosmotech/organization/repositories/OrganizationRepository.kt
@@ -8,8 +8,8 @@ import com.redis.om.spring.repository.RedisDocumentRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
-@Repository interface OrganizationRepository : RedisDocumentRepository<Organization, String> {
+@Repository
+interface OrganizationRepository : RedisDocumentRepository<Organization, String> {
   @Query("(-@security_default:{none}) | (@security_accessControlList_id:{\$userId})")
   fun findOrganizationsBySecurity(@Param("userId") userId: String): List<Organization>
-
 }
