e5e7e9f16fa462f5706ed8bec85cc6d3c3c9afd9
jreynard <jeremy.reynard@cosmotech.com>
2023-01-17 09:18:00 +0100

    Handle Json string twin property correctly

--- twingraph/src/main/kotlin/com/cosmotech/twingraph/utils/TwingraphUtils.kt
+++ twingraph/src/main/kotlin/com/cosmotech/twingraph/utils/TwingraphUtils.kt
@@ -3,29 +3,39 @@
 package com.cosmotech.twingraph.utils
 
 import com.cosmotech.api.utils.objectMapper
+import com.fasterxml.jackson.core.JacksonException
+import com.fasterxml.jackson.module.kotlin.readValue
 import redis.clients.jedis.graph.entities.Node
-import redis.clients.jedis.graph.entities.Property
 
 object TwingraphUtils {
 
   private const val NODE_ID_PROPERTY_NAME = "id"
 
+  @Suppress("SwallowedException")
   @JvmStatic
   fun getNodeJson(node: Node): String {
     var nodeId = node.id.toString()
-
     if (node.entityPropertyNames.contains(NODE_ID_PROPERTY_NAME)) {
       nodeId = node.getProperty(NODE_ID_PROPERTY_NAME).value.toString()
     }
+    val jsonObjectMapper = objectMapper()
 
-    val graphNode =
-        GraphNode(
-            node.getLabel(0),
-            nodeId,
-            node.entityPropertyNames.filter { it != NODE_ID_PROPERTY_NAME }.map {
-              node.getProperty(it)
-            })
-    return objectMapper().writeValueAsString(graphNode)
+    val properties = mutableMapOf<String, Any>()
+    node.entityPropertyNames.filter { it != NODE_ID_PROPERTY_NAME }.map { propertyName ->
+      val propertyValue = node.getProperty(propertyName).value
+      if (propertyValue is String) {
+        try {
+          properties[propertyName] = jsonObjectMapper.readValue(propertyValue)
+        } catch (e: JacksonException) {
+          properties[propertyName] = propertyValue
+        }
+      } else {
+        properties[propertyName] = propertyValue
+      }
+    }
+
+    val graphNode = GraphNode(node.getLabel(0), nodeId, properties)
+    return jsonObjectMapper.writeValueAsString(graphNode)
   }
 
   @JvmStatic
@@ -37,4 +47,4 @@ object TwingraphUtils {
   }
 }
 
-data class GraphNode(var label: String, var id: String, var properties: List<Property<Any>>)
+data class GraphNode(var label: String, var id: String, var properties: MutableMap<String, Any>)
