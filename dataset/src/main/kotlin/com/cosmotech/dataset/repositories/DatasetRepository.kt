c83a7d86c944f63de2bbf4659b6cc9745909de66
Cramer <2lcramer@gmail.com>
2023-01-17 11:27:53 +0100

    migrate dataset service to redis

---
+++ dataset/src/main/kotlin/com/cosmotech/dataset/repositories/DatasetRepository.kt
@@ -0,0 +1,20 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.organization.repositories
+
+import com.cosmotech.dataset.domain.Dataset
+import com.redis.om.spring.annotations.Query
+import com.redis.om.spring.repository.RedisDocumentRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DatasetRepository : RedisDocumentRepository<Dataset, String> {
+  @Query("@organization_id:{\$organizationId")
+  fun findDatasetByOrganization(@Param("organizationId") organizationId: String): List<Dataset>
+
+  @Query("@tags:{\$tags}") fun findDatasetByTags(@Param("tags") tags: Set<String>): List<Dataset>
+
+  @Query("@connector_id:{\$connectorId}")
+  fun findDatasetByConnectorId(@Param("connectorId") connectorId: String): List<Dataset>
+}
