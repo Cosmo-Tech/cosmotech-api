cc86dce2adbd5f729fffbe67c61818b7a36dd265
Michel Guillon <michel.guillon@cosmotech.com>
2023-01-18 15:07:49 +0100

    Refacto code for connector module

--- connector/src/main/kotlin/com/cosmotech/connector/redis/ConnectorRepository.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Cosmo Tech.
-// Licensed under the MIT license.
-package com.cosmotech.connector.redis
-
-import com.cosmotech.connector.domain.Connector
-import com.redis.om.spring.repository.RedisDocumentRepository
-import org.springframework.stereotype.Repository
-
-@Repository interface ConnectorRepository : RedisDocumentRepository<Connector, String>
