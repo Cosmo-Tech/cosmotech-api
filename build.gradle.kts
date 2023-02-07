624aca7019321775d3ae447bdead0aad53d62666
Vincent Carluer <vcarluer@gmail.com>
2023-02-07 16:12:41 +0100

    Missing configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -239,6 +239,14 @@ subprojects {
               "See http://www.slf4j.org/faq.html#changesInVersion200")
     }
 
+    api("com.github.Cosmo-Tech:cosmotech-api-azure-cosmos-db:$cosmotechApiCosmosDBVersion") {
+      exclude(group = "org.slf4j", module = "slf4j-api")
+      because(
+          "this depends on org.slf4j:slf4j-api 1.8.0-beta4 (pre 2.x)," +
+              "which is not backward-compatible with 1.7.x." +
+              "See http://www.slf4j.org/faq.html#changesInVersion200")
+    }
+
     implementation(platform("com.azure.spring:azure-spring-boot-bom:$azureSpringBootBomVersion"))
     api("com.azure.spring:azure-spring-boot-starter-cosmos")
   }
