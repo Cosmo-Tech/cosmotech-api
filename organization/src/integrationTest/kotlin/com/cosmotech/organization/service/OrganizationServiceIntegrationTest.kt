6012b4494b3a6c07a8d2607117ea2f68f806f042
Cramer <leopold.h.cramer@gmail.com>
2023-02-14 16:34:17 +0100

    some changes

--- organization/src/integrationTest/kotlin/com/cosmotech/organization/service/OrganizationServiceIntegrationTest.kt
+++ organization/src/integrationTest/kotlin/com/cosmotech/organization/service/OrganizationServiceIntegrationTest.kt
@@ -4,16 +4,19 @@ package com.cosmotech.organization.service
 
 import com.cosmotech.api.tests.CsmRedisTestBase
 import com.cosmotech.api.utils.getCurrentAuthenticatedMail
+import com.cosmotech.api.utils.getCurrentAuthenticatedRoles
 import com.cosmotech.api.utils.getCurrentAuthenticatedUserName
 import com.cosmotech.organization.api.OrganizationApiService
 import com.cosmotech.organization.domain.Organization
 import com.cosmotech.organization.domain.OrganizationAccessControl
 import com.cosmotech.organization.domain.OrganizationSecurity
+import com.redis.om.spring.RediSearchIndexer
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockkStatic
 import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -34,6 +37,8 @@ class OrganizationServiceIntegrationTest : CsmRedisTestBase() {
 
   private val logger = LoggerFactory.getLogger(OrganizationServiceIntegrationTest::class.java)
 
+  @Autowired lateinit var rediSearchIndexer: RediSearchIndexer
+
   @Autowired lateinit var organizationApiService: OrganizationApiService
 
   @BeforeEach
@@ -41,31 +46,40 @@ class OrganizationServiceIntegrationTest : CsmRedisTestBase() {
     mockkStatic("com.cosmotech.api.utils.SecurityUtilsKt")
     every { getCurrentAuthenticatedMail(any()) } returns "test.user@cosmotech.com"
     every { getCurrentAuthenticatedUserName() } returns "test.user"
+    every { getCurrentAuthenticatedRoles(any()) } returns listOf()
+    rediSearchIndexer.createIndexFor(Organization::class.java)
+  }
+
+  fun mockOrganization(id: String, name: String): Organization {
+    return Organization(
+        id = id,
+        name = name,
+        ownerId = "my.account-tester@cosmotech.com",
+        security =
+            OrganizationSecurity(
+                default = "none",
+                accessControlList =
+                    mutableListOf(
+                        OrganizationAccessControl(
+                            id = "my.account-tester2@cosmotech.com", role = "reader"),
+                        OrganizationAccessControl(
+                            id = "my.account-tester@cosmotech.com", role = "admin"))))
   }
 
   @Test
   fun test_register_organization() {
-    val organization =
-        Organization(
-            id = "o-organization-test",
-            name = "Organization test",
-            ownerId = "my.account-tester@cosmotech.com",
-            security =
-                OrganizationSecurity(
-                    default = "none",
-                    accessControlList =
-                        mutableListOf(
-                            OrganizationAccessControl(
-                                id = "my.account-tester2@cosmotech.com", role = "reader"),
-                            OrganizationAccessControl(
-                                id = "my.account-tester@cosmotech.com", role = "admin"))))
-
+    val organization1 = mockOrganization("o-organization-test-1", "Organization-test-1")
+    val organization2 = mockOrganization("o-organization-test-2", "Organization-test-2")
     logger.info("Create new organization...")
-    val organizationRegistered = organizationApiService.registerOrganization(organization)
-    logger.info("New organization created : ${organizationRegistered.id}")
+    val organizationRegistered1 = organizationApiService.registerOrganization(organization1)
+    val organizationRegistered2 = organizationApiService.registerOrganization(organization2)
+    logger.info("New organization created : ${organizationRegistered1.id}")
     logger.info("Fetch new organization created...")
     val organizationRetrieved =
-        organizationApiService.findOrganizationById(organizationRegistered.id!!)
-    assertEquals(organizationRegistered, organizationRetrieved)
+        organizationApiService.findOrganizationById(organizationRegistered1.id!!)
+    assertEquals(organizationRegistered1, organizationRetrieved)
+    logger.info("Fetch all Organizations...")
+    val organizationList = organizationApiService.findAllOrganizations()
+    assertTrue(organizationList.size == 2)
   }
 }
