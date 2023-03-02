95947f3200cec9123dc9f0e2f188f41af027e6ab
Michel Guillon <michel.guillon@cosmotech.com>
2023-03-02 15:34:07 +0100

    WIP use containersettemplate to run containers in one pod

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/workflow/argo/WorkflowBuilders.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/workflow/argo/WorkflowBuilders.kt
@@ -8,11 +8,14 @@ import com.cosmotech.scenariorun.container.BASIC_SIZING
 import com.cosmotech.scenariorun.container.getLimitsMap
 import com.cosmotech.scenariorun.container.getRequestsMap
 import com.cosmotech.scenariorun.container.toContainerResourceSizing
+import com.cosmotech.scenariorun.domain.ContainerResourceSizing
 import com.cosmotech.scenariorun.domain.ScenarioRunContainer
 import com.cosmotech.scenariorun.domain.ScenarioRunStartContainers
 import com.cosmotech.scenariorun.getNodeLabelSize
 import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1ArchiveStrategy
 import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1Artifact
+import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1ContainerNode
+import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1ContainerSetTemplate
 import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1DAGTask
 import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1DAGTemplate
 import io.argoproj.workflow.models.IoArgoprojWorkflowV1alpha1Metadata
@@ -106,19 +109,162 @@ internal fun buildTemplate(
   return template
 }
 
+private fun buildVolumeMount(): List<V1VolumeMount> {
+  return listOf(
+      V1VolumeMount()
+          .name(VOLUME_CLAIM)
+          .mountPath(VOLUME_DATASETS_PATH)
+          .subPath(VOLUME_CLAIM_DATASETS_SUBPATH),
+      V1VolumeMount()
+          .name(VOLUME_CLAIM)
+          .mountPath(VOLUME_PARAMETERS_PATH)
+          .subPath(VOLUME_CLAIM_PARAMETERS_SUBPATH),
+      V1VolumeMount().name("out").mountPath("/var/csmoutput"))
+}
+
+private fun buildEnvVar(container: ScenarioRunContainer): List<V1EnvVar> {
+  var envVars: MutableList<V1EnvVar> = mutableListOf()
+  if (container.envVars != null) {
+    container.envVars.forEach { (key, value) ->
+      val envVar = V1EnvVar().name(key).value(value)
+      envVars.add(envVar)
+    }
+  }
+  return envVars
+}
+
+private fun buildDependencies(
+    container: ScenarioRunContainer,
+    previousContainer: ScenarioRunContainer?
+): List<String> {
+  var dependencies: List<String> = mutableListOf()
+  if (container.dependencies != null) {
+    if (CSM_DAG_ROOT !in container.dependencies) {
+      dependencies = container.dependencies.map { it.lowercase() }
+    }
+  } else {
+    if (previousContainer != null) dependencies = listOf(previousContainer.name)
+  }
+  return dependencies
+}
+
+private fun buildContainerNode(
+    container: ScenarioRunContainer,
+    command: List<String>,
+    imagePullPolicy: String,
+    envVars: List<V1EnvVar>,
+    dependencies: List<String>,
+    sizingInfo: ContainerResourceSizing
+): IoArgoprojWorkflowV1alpha1ContainerNode {
+  val containerNode =
+      IoArgoprojWorkflowV1alpha1ContainerNode()
+          .name(container.name.lowercase())
+          .image(container.image)
+          .imagePullPolicy(imagePullPolicy)
+          .command(command)
+          .env(envVars)
+          .args(container.runArgs)
+          .dependencies(dependencies)
+          .resources(
+              V1ResourceRequirements()
+                  .requests(sizingInfo.getRequestsMap())
+                  .limits(sizingInfo.getLimitsMap()))
+  if (container.entrypoint != null) {
+    containerNode.command(listOf(container.entrypoint))
+  }
+  return containerNode
+}
+
+@Suppress("LongMethod", "MaxLineLength")
+internal fun buildContainerSetTemplate(
+    startContainers: ScenarioRunStartContainers,
+    csmPlatformProperties: CsmPlatformProperties
+): MutableList<IoArgoprojWorkflowV1alpha1Template> {
+  var commandMap: Map<String, List<String>> =
+      mutableMapOf(
+          "fetchDatasetContainer-1" to
+              listOf(
+                  "java",
+                  "-cp",
+                  "/app/resources:/app/classes:/app/libs/*",
+                  "com.cosmotech.ApplicationKt"),
+          "fetchScenarioParametersContainer" to listOf("/bin/sh", "-c", "node", "."),
+          "applyParametersContainer" to listOf("/bin/sh", "-c", "node", "."),
+          "sendDataWarehouseContainer" to
+              listOf(
+                  "java",
+                  "-cp",
+                  "/app/resources:/app/classes:/app/libs/*",
+                  "com.cosmotech.connector.adx.ApplicationKt"),
+          "runContainer" to listOf("/bin/sh", "-c", "node", "."))
+
+  var containerSet = IoArgoprojWorkflowV1alpha1ContainerSetTemplate()
+
+  containerSet.volumeMounts(buildVolumeMount())
+  var previousContainer: ScenarioRunContainer? = null
+  for (srContainer in startContainers.containers) {
+    var envVars = buildEnvVar(srContainer)
+
+    val sizingInfo = srContainer.runSizing ?: BASIC_SIZING.toContainerResourceSizing()
+
+    val dependencies = buildDependencies(srContainer, previousContainer)
+    previousContainer = srContainer
+
+    srContainer.nodeLabel
+    val containerNode =
+        buildContainerNode(
+            srContainer,
+            commandMap[srContainer.name]!!,
+            csmPlatformProperties.argo.imagePullPolicy,
+            envVars,
+            dependencies.map { it.lowercase() },
+            sizingInfo)
+
+    containerSet.containers.add(containerNode)
+  }
+
+  val template =
+      IoArgoprojWorkflowV1alpha1Template()
+          .name(CSM_DAG_ENTRYPOINT)
+          .metadata(
+              IoArgoprojWorkflowV1alpha1Metadata().labels(startContainers.containers[0].labels))
+          .containerSet(containerSet)
+          .nodeSelector(startContainers.containers[0].getNodeLabelSize())
+          .addVolumesItem(V1Volume().emptyDir(V1EmptyDirVolumeSource()).name("out"))
+
+  val artifacts =
+      startContainers.containers[0].artifacts?.map {
+        IoArgoprojWorkflowV1alpha1Artifact()
+            .name(it.name)
+            .path("/var/csmoutput/${it.path}")
+            .archive(IoArgoprojWorkflowV1alpha1ArchiveStrategy().none(Object()))
+      }
+  if (!artifacts.isNullOrEmpty()) {
+    template.outputs(IoArgoprojWorkflowV1alpha1Outputs().artifacts(artifacts))
+  }
+  var templates: MutableList<IoArgoprojWorkflowV1alpha1Template> = mutableListOf()
+  templates.add(template)
+  return templates
+}
+
 internal fun buildWorkflowSpec(
     csmPlatformProperties: CsmPlatformProperties,
     startContainers: ScenarioRunStartContainers,
     executionTimeout: Int?
 ): IoArgoprojWorkflowV1alpha1WorkflowSpec {
   val nodeSelector = mutableMapOf("kubernetes.io/os" to "linux", "cosmotech.com/tier" to "compute")
-  val templates =
-      startContainers
-          .containers
-          .map { container -> buildTemplate(container, csmPlatformProperties) }
-          .toMutableList()
-  val entrypointTemplate = buildEntrypointTemplate(startContainers)
-  templates.add(entrypointTemplate)
+  var templates: MutableList<IoArgoprojWorkflowV1alpha1Template> = mutableListOf()
+  if (csmPlatformProperties.argo.useContainerSetTemplate) {
+    templates = buildContainerSetTemplate(startContainers, csmPlatformProperties)
+  } else {
+    templates =
+        startContainers
+            .containers
+            .map { container -> buildTemplate(container, csmPlatformProperties) }
+            .toMutableList()
+    val entrypointTemplate = buildEntrypointTemplate(startContainers)
+    templates.add(entrypointTemplate)
+  }
 
   var workflowSpec =
       IoArgoprojWorkflowV1alpha1WorkflowSpec()
@@ -153,6 +299,7 @@ internal fun buildWorkflow(
                 .labels(startContainers.labels))
         .spec(buildWorkflowSpec(csmPlatformProperties, startContainers, executionTimeout))
 
+@Suppress("UnusedPrivateMember")
 private fun buildEntrypointTemplate(
     startContainers: ScenarioRunStartContainers
 ): IoArgoprojWorkflowV1alpha1Template {
@@ -200,3 +347,47 @@ private fun buildVolumeClaims(
                           .requests(workflowsConfig.requests.mapValues { Quantity(it.value) })))
   return listOf(dataDir)
 }
+
+
+
+
+
+
+// cat <<EOF > redis-configmap.yaml
+// +apiVersion: v1
+// +kind: ConfigMap
+// +metadata:
+// +  name: redis-ss-configuration
+// +  namespace: ${NAMESPACE}
+// +  labels:
+// +    app: cosmotechredis
+// +data:
+// +  master.conf: |
+// +    maxmemory 400mb
+// +    maxmemory-policy allkeys-lru
+// +    maxclients 20000
+// +    timeout 300
+// +    appendonly no
+// +    dbfilename dump.rdb
+// +    dir /data
+//
+//
+//
+// cat <<EOF > values-redis.yaml
+// auth:
+// password: ${REDIS_PASSWORD}
+// image:
+// registry: ghcr.io
+// repository: cosmo-tech/cosmotech-redis
+// tag: ${VERSION_REDIS_COSMOTECH}
+// extraDeploy:
+// - apiVersion: v1
+// kind: ConfigMap
+// metadata:
+// name: cosmotech-redis-configmap
+// data:
+// redis-config: |
+// maxmemory ${REDIS_MEMORY_MAX}
+// maxmemory-policy allkeys-lru
+// master:
+// persistence:
