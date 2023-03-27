f07662d6b1a11632a13aea59c9564038755cac8c
icreated <sergey.polyarus.ext@cosmotech.com>
2023-03-27 16:03:30 +0200

    Improvements

--- api/kubernetes/redis_dump.sh
+++ api/kubernetes/redis_dump.sh
@@ -4,16 +4,17 @@
 # The script connects to the redis deployed in the kubernetes cluster.
 
 usage() {
-  echo "usage: $0 [-s] [-r] [-x] [-n namespace] [-d redis_pod] [-h redis_host] [-a redis_password] [-p redis_port] [-f dump_file]"
+  echo "usage: $0 [-s] [-r] [-x] [-n namespace] [-d redis_pod] [-h redis_host] [-a redis_password] [-p redis_port]"
   echo "  -s: save redis database to dump file"
   echo "  -r: restore redis database from dump file"
   echo "  -x: get redis password from secret"
   echo "  -n: namespace where redis is deployed (default: phoenix)"
-  echo "  -d: redis pod name (default: cosmotechredis-master)"
+  echo "  -d: redis pod name (default: cosmotechredis-master-0)"
   echo "  -h: redis host (default: localhost)"
   echo "  -a: redis password (default: get from secret)"
   echo "  -p: redis port (default: 6379)"
-  echo "  -f: dump file (default: /tmp/dump.rdb)"
+  echo "  -f: redis dump file (default: /tmp/dump.rdb)"
+  echo "  -y: do not ask for confirmation"
   echo "example backuping Redis dump: $0 -s -f /tmp/dump.rdb"
   echo "example restoring Redis dump: $0 -r -f /tmp/dump.rdb"
   echo "example getting Redis password from secret: $0 -x"
@@ -34,23 +35,36 @@ check_error() {
   return 0
 }
 
-save_database() {
-  echo "Saving Redis database to ${DB_DUMP}"
-  FOUND_POD=$(kubectl get pods -n ${NAMESPACE} | grep ${REDIS_POD} | awk '{print $1}')
+check_prerequisites() {
+  DUMP_USER=$(stat -c '%U' ${REDIS_MOUNT_DUMP})
+  if [[ $DUMP_USER == "root" ]]; then
+    echo "ERROR: Redis dump file ${REDIS_MOUNT_DUMP} is owned by root user, please change the owner to the current user"
+    exit 1
+  fi
+
+  CURRENT_CONTEXT=$(kubectl config current-context)
+  echo "Current context: ${CURRENT_CONTEXT}"
+
+  FOUND_POD=$(kubectl get pod --no-headers -n ${NAMESPACE} ${REDIS_POD} | awk '{print $1}')
   check_not_empty "$FOUND_POD" "Redis pod ${REDIS_POD} not found on namespace ${NAMESPACE}"
+  echo "Redis pod found: ${FOUND_POD}"
 
   if [ -z "$REDIS_PASSWORD" ]; then
     echo "Redis password not provided, trying to get it from secret"
     get_redis_password
   fi
+}
+
+save_database() {
+  echo "Saving Redis database to ${DB_DUMP}"
+  check_prerequisites
 
   local redis_connect="redis-cli --no-auth-warning -h ${REDIS_HOST} -p ${REDIS_PORT} -a ${REDIS_PASSWORD}"
-  echo "Connecting to Redis: ${redis_connect}"
-  RESULT=$(kubectl exec -it ${FOUND_POD} -n ${NAMESPACE} -- ${redis_connect} save)
+  echo "Connecting to Redis: ${redis_connect} from context $CURRENT_CONTEXT - ${NAMESPACE}/${FOUND_POD}"
+  RESULT=$(kubectl exec -it ${FOUND_POD} -n ${NAMESPACE} -- ${redis_connect} bgsave)
   check_error "$RESULT" "NOAUTH" "Authentication to Redis failed, wrong password: ${REDIS_PASSWORD}"
   check_error "$RESULT" "Connection refused" "ERROR: ${RESULT}"
   check_error "$RESULT" "ERR" "ERROR: ${RESULT}"
-
   [ -e "${DB_DUMP}" ] && rm -f "${DB_DUMP}"
   RESULT=$(kubectl cp ${NAMESPACE}/${FOUND_POD}:data/dump.rdb ${DB_DUMP})
   check_error "$RESULT" "error" "Error while copying Redis dump: ${RESULT}"
@@ -59,21 +73,12 @@ save_database() {
 
 
 restore_database() {
-
   [ ! -f "${DB_DUMP}" ] && echo "ERROR: Redis dump file not found: ${DB_DUMP}" && exit 1
 
   echo "Restore Redis database from ${DB_DUMP}"w
-  FOUND_POD=$(kubectl get pods -n ${NAMESPACE} | grep ${REDIS_POD} | awk '{print $1}')
-  check_not_empty "$FOUND_POD" "Redis pod ${REDIS_POD} not found on namespace ${NAMESPACE}"
-
-  if [ -z "$REDIS_PASSWORD" ]; then
-    echo "Redis password not provided, trying to get it from secret"
-    get_redis_password
-  fi
+  check_prerequisites
 
   # Check if we are in local current context
-  CURRENT_CONTEXT=$(kubectl config current-context)
-  echo "Current context: ${CURRENT_CONTEXT}"
   if [[ $CURRENT_CONTEXT == kind* ]]; then
     echo "We are in local context: $CURRENT_CONTEXT"
     echo "Redis dump will be restored by simply copying the dump file to the local Redis data folder"
@@ -83,12 +88,13 @@ restore_database() {
     exit 0
   fi
 
-  read -p "We are in remote context: $CURRENT_CONTEXT. Are you sure to restore the database Redis from ${DB_DUMP}? (yes / no) : " response
-  if [[ $response != "yes" ]]; then
-    echo "Exit from script"
-    exit 0
+  if [[ $COMPLETION == "false" ]]; then
+    read -p "We are in remote context: $CURRENT_CONTEXT. Are you sure to restore the database Redis from ${DB_DUMP}? (yes / no) : " response
+    if [[ $response != "yes" ]]; then
+      echo "Exit from script"
+      exit 0
+    fi
   fi
-
 
   PVC=$(kubectl -n $NAMESPACE describe pod $FOUND_POD | grep ClaimName | awk '{print $2}')
   check_not_empty "$PVC" "Error while getting Redis PVC name: ${PVC}"
@@ -160,25 +166,28 @@ restart_redis() {
 unset ACTION DB_DUMP NAMESPACE REDIS_HOST REDIS_POD REDIS_PORT REDIS_PASSWORD
 ACTION="HELP"
 DB_DUMP="/tmp/dump.rdb"
+REDIS_MOUNT_DUMP="$HOME/data/dump.rdb"
 NAMESPACE="phoenix"
 REDIS_HOST="localhost"
-REDIS_POD="cosmotechredis-master"
+REDIS_POD="cosmotechredis-master-0"
 REDIS_PORT="6379"
 REDIS_PASSWORD=""
+COMPLETION=false
 
 
-while getopts 'srxhn:d:h:a:p:f:' args
+while getopts 'srxyhn:d:h:a:p:f:' args
 do
   case $args in
     s) ACTION=SAVE ;;
     r) ACTION=RESTORE ;;
     x) ACTION=PASSWORD ;;
+    y) COMPLETION=true ;;
     n) NAMESPACE=$OPTARG ;;
     d) REDIS_POD=$OPTARG ;;
     h) REDIS_HOST=$OPTARG ;;
+    f) DB_DUMP=$OPTARG ;;
     a) REDIS_PASSWORD=$OPTARG ;;
     p) REDIS_PORT=$OPTARG ;;
-    f) DB_DUMP=$OPTARG ;;
     *) echo usage
        exit 1 ;;
   esac
