83938ac0ee8c98ab08490d63772ca005d55e5bbf
Leopold Cramer <leopold.h.cramer@gmail.com>
2023-04-05 15:52:21 +0200

    add completed tests for twingraph CRUD

--- twingraph/src/integrationTest/kotlin/com/cosmotech/twingraph/service/TwingraphServiceIntegrationTest.kt
+++ twingraph/src/integrationTest/kotlin/com/cosmotech/twingraph/service/TwingraphServiceIntegrationTest.kt
@@ -5,14 +5,16 @@ package com.cosmotech.twingraph.service
 import com.cosmotech.api.tests.CsmRedisTestBase
 import com.cosmotech.twingraph.api.TwingraphApiService
 import com.redis.testcontainers.RedisStackContainer
+import com.redis.testcontainers.junit.RedisTestContext
+import com.redis.testcontainers.junit.RedisTestContextsSource
+import com.redislabs.redisgraph.ResultSet
 import io.mockk.junit5.MockKExtension
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
 import org.junit.runner.RunWith
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,7 +24,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.util.ReflectionTestUtils
 import redis.clients.jedis.JedisPool
-import redis.clients.jedis.exceptions.JedisDataException
 
 @ActiveProfiles(profiles = ["twingraph-test"])
 @ExtendWith(MockKExtension::class)
@@ -35,97 +36,106 @@ class TwingraphServiceIntegrationTest : CsmRedisTestBase() {
 
   @Autowired lateinit var twingraphApiService: TwingraphApiService
 
-  val graphId = "graph:lastVersion"
-//TODO
-//TODO
-//TODO
-//TODO
-
-
-
-//TODO      WHEN GETTING METADATA DO SOMETHING IF NO GRAPH EXIST
-
-
-
-//TODO
-//TODO
-//TODO
-//TODO
+  val graphId = "graph"
   @BeforeEach fun setUp() {}
 
-  @Test
-  fun TwingraphTest() {
+  @RedisTestContextsSource
+  @ParameterizedTest
+  fun TwingraphTest(context: RedisTestContext) {
 
-    val context = getContext(redisStackServer)
     val containerIp =
         (context.server as RedisStackContainer).containerInfo.networkSettings.ipAddress
     println(containerIp)
     ReflectionTestUtils.setField(twingraphApiService, "csmJedisPool", JedisPool(containerIp, 6379))
 
-    var nodeStart = listOf<Any>()
-    var nodeResult = listOf<Any>()
-    var relationshipStart = listOf<Any>()
-    var relationshipResult = listOf<Any>()
-
-    //      every { TwinGraph(any(), any()) }
+    context.sync().hset(graphId + "MetaData", mapOf("lastVersion" to "1"))
 
     logger.info("Create Nodes")
-    nodeStart = twingraphApiService.createNodes(
-        "orga", graphId, listOf(mapOf("name" to "node_a", "params" to "size:0,length:0")))
+    val nodeStart =
+        (twingraphApiService
+                .createNodes(
+                    "orga",
+                    graphId,
+                    listOf(mapOf("type" to "node", "name" to "node_a", "params" to "size:0")))
+                .first() as
+                ResultSet)
+            .next()
+            .values()
+    assertNotEquals(listOf<Any>(), nodeStart)
 
     logger.info("Read Nodes")
-    nodeResult = twingraphApiService.getNodes("orga", graphId, listOf("node_a"))
+    var nodeResult =
+        (twingraphApiService.getNodes("orga", graphId, listOf("node_a")).first() as ResultSet)
+            .next()
+            .values()
     assertEquals(nodeStart, nodeResult)
 
     logger.info("Create Relationships")
     twingraphApiService.createNodes(
-        "orga", graphId, listOf(mapOf("name" to "node_b", "params" to "size:1,length:1")))
-    relationshipStart = twingraphApiService.createRelationships(
-        "orga",
-        graphId,
-        listOf(
-            mapOf(
-                "source" to "node_a",
-                "target" to "node_b",
-                "name" to "relationship_a",
-            "params" to "duration: 0")))
+        "orga", graphId, listOf(mapOf("type" to "node", "name" to "node_b", "params" to "size:1")))
+    val relationshipStart =
+        (twingraphApiService
+                .createRelationships(
+                    "orga",
+                    graphId,
+                    listOf(
+                        mapOf(
+                            "type" to "relationship",
+                            "source" to "node_a",
+                            "target" to "node_b",
+                            "name" to "relationship_a",
+                            "params" to "duration:0")))
+                .first() as
+                ResultSet)
+            .next()
+            .values()
 
     logger.info("Read Relationships")
-    relationshipResult =
-        twingraphApiService.getRelationships("orga", graphId, listOf("relationship_a"))
+    var relationshipResult =
+        (twingraphApiService.getRelationships("orga", graphId, listOf("relationship_a")).first() as
+                ResultSet)
+            .next()
+            .values()
     assertEquals(relationshipStart, relationshipResult)
 
     logger.info("Update Nodes")
-    twingraphApiService.updateNodes(
-        "orga", graphId, listOf(mapOf("name" to "node_a", "params" to "size:2,length:2")))
-    nodeResult = twingraphApiService.getNodes("orga", graphId, listOf("node_a"))
+    nodeResult =
+        (twingraphApiService
+                .updateNodes(
+                    "orga", graphId, listOf(mapOf("name" to "node_a", "params" to "size:2")))
+                .first() as
+                ResultSet)
+            .next()
+            .values()
     assertNotEquals(nodeStart, nodeResult)
 
     logger.info("Update Relationships")
-    twingraphApiService.updateRelationships(
-        "orga",
-        graphId,
-        listOf(
-            mapOf(
-                "source" to "node_a",
-                "target" to "node_b",
-                "name" to "relationship_a",
-                "params" to "duration: 2")))
     relationshipResult =
-        twingraphApiService.getRelationships("orga", graphId, listOf("relationship_a"))
+        (twingraphApiService
+                .updateRelationships(
+                    "orga",
+                    graphId,
+                    listOf(
+                        mapOf(
+                            "source" to "node_a",
+                            "target" to "node_b",
+                            "name" to "relationship_a",
+                            "params" to "duration:2")))
+                .first() as
+                ResultSet)
+            .next()
+            .values()
     assertNotEquals(relationshipStart, relationshipResult)
 
     logger.info("Delete Relationships")
     twingraphApiService.deleteRelationships(
         "orga", "graph:0", listOf(mapOf("name" to "relationship_a")))
-    assertThrows<JedisDataException> {
-        twingraphApiService.getRelationships("orga", graphId, listOf("relationship_a"))
+    assertDoesNotThrow {
+      twingraphApiService.getRelationships("orga", graphId, listOf("relationship_a"))
     }
 
     logger.info("Delete Nodes")
     twingraphApiService.deleteNodes("orga", graphId, listOf(mapOf("name" to "node_a")))
-      assertThrows<JedisDataException> {
-          twingraphApiService.getNodes("orga", graphId, listOf("node_a"))
-      }
+    assertDoesNotThrow { twingraphApiService.getNodes("orga", graphId, listOf("node_a")) }
   }
 }
