1595bd1092eda3ee69d31226857aab1987099c67
Leopold Cramer <leopold.h.cramer@gmail.com>
2023-04-24 15:09:21 +0200

    add integrationTests for createGraph

--- twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/BulkQuery.kt
+++ twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/BulkQuery.kt
@@ -1,71 +1,73 @@
-class BulkQuery private constructor(private val graphName: String, private val begin: Boolean, private val typeNodesList: List<TypeNodes>, private val typeEdgesList: List<TypeEdges>) {
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.twingraph.bulk
+class BulkQuery
+private constructor(
+    private val graphName: String,
+    private val begin: Boolean,
+    private val typeNodesList: List<TypeNodes>,
+    private val typeEdgesList: List<TypeEdges>
+) {
 
-    data class Builder(
-        private var graphName: String? = null,
-        private var begin: Boolean = false,
-        private var typeNodes: Map<String, TypeNodes> = mapOf(),
-        private var typeEdges: Map<String, TypeEdges> = mapOf()
-    ) {
-        fun graphName(name: String) = apply { this.graphName = name }
-        fun first() = apply { this.begin = true }
+  data class Builder(
+      private var graphName: String? = null,
+      private var begin: Boolean = false,
+      private var typeNodes: Map<String, TypeNodes> = mapOf(),
+      private var typeEdges: Map<String, TypeEdges> = mapOf()
+  ) {
+    fun graphName(name: String) = apply { this.graphName = name }
+    fun first() = apply { this.begin = true }
 
-        fun addTypeNode(type: String, typeNode: TypeNodes) = apply {
-            if (type !in typeNodes)
-                this.typeNodes += type to typeNode
-        }
-        fun addNodeToTypeNode(type: String, node: Node) = apply {
-            if (type in this.typeNodes)
-                this.typeNodes[type]?.addNodes(node)
-        }
-
-        fun addTypeEdge(type: String, typeEdge: TypeEdges) = apply {
-            if (type !in typeEdges)
-                this.typeEdges += type to typeEdge
-        }
-        fun addEdgeToTypeEdge(type: String, edge: Edge) = apply {
-            if (type in this.typeEdges)
-                this.typeEdges[type]?.addEdges(edge)
-        }
-        // For ease, those sizes are not calculated as there are very small
-        //   GRAPH.BULK     string to byteArray size
-        //   graphName      string to byteArray size
-        //   BEGIN          string to byteArray size
-        //   nodeCount      string to byteArray size
-        //   nodeTypeCount  string to byteArray size
-        //   edgeCount      string to byteArray size
-        //   edgtypeCount   string to byteArray size
-        fun size() : Int {
-            return typeNodes.values.sumOf { it.size } + typeEdges.values.sumOf { it.size }
-        }
-
-        fun build() = BulkQuery(graphName!!, begin, typeNodes.values.toList(), typeEdges.values.toList())
+    fun addTypeNode(type: String, typeNode: TypeNodes) = apply {
+      if (type !in typeNodes) this.typeNodes += type to typeNode
+    }
+    fun addNodeToTypeNode(type: String, node: Node) = apply {
+      if (type in this.typeNodes) this.typeNodes[type]?.addNodes(node)
     }
 
-    fun generate_query_args(): Array<ByteArray> {
-        var args: Array<ByteArray> = arrayOf()
-
-        args += graphName.toByteArray()
-        if (begin)
-            args += "BEGIN".toByteArray()
-        // count of node and edge to create
-        args += typeNodesList.map { it.nodesBinaryBlobFormat }.flatten().size.toString().toByteArray()
-        args += typeEdgesList.map { it.edgesBinaryBlobFormat }.flatten().size.toString().toByteArray()
-        // count of node type and reltype
-        args += typeNodesList.size.toString().toByteArray()
-        args += typeEdgesList.size.toString().toByteArray()
-
-        this.typeNodesList.forEach {
-            args += it.toBinary()
-        }
-        this.typeEdgesList.forEach {
-            args += it.toBinary()
-        }
-
-        return args
+    fun addTypeEdge(type: String, typeEdge: TypeEdges) = apply {
+      if (type !in typeEdges) this.typeEdges += type to typeEdge
+    }
+    fun addEdgeToTypeEdge(type: String, edge: Edge) = apply {
+      if (type in this.typeEdges) this.typeEdges[type]?.addEdges(edge)
+    }
+    // For ease, those sizes are not calculated as there are very small
+    //   GRAPH.BULK     string to byteArray size
+    //   graphName      string to byteArray size
+    //   BEGIN          string to byteArray size
+    //   nodeCount      string to byteArray size
+    //   nodeTypeCount  string to byteArray size
+    //   edgeCount      string to byteArray size
+    //   edgtypeCount   string to byteArray size
+    fun size(): Int {
+      return typeNodes.values.sumOf { it.size } + typeEdges.values.sumOf { it.size }
     }
 
-    private fun ByteArray.toHex(): String = joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
-    override fun toString(): String {
-        return generate_query_args().joinToString(separator = " - ") { bytes -> bytes.toHex() }
-    }
+    fun build() =
+        BulkQuery(graphName!!, begin, typeNodes.values.toList(), typeEdges.values.toList())
+  }
+
+  fun generateQueryArgs(): Array<ByteArray> {
+    var args: Array<ByteArray> = arrayOf()
+
+    args += graphName.toByteArray()
+    if (begin) args += "BEGIN".toByteArray()
+    // count of node and edge to create
+    args += typeNodesList.map { it.nodesBinaryBlobFormat }.flatten().size.toString().toByteArray()
+    args += typeEdgesList.map { it.edgesBinaryBlobFormat }.flatten().size.toString().toByteArray()
+    // count of node type and reltype
+    args += typeNodesList.size.toString().toByteArray()
+    args += typeEdgesList.size.toString().toByteArray()
+
+    this.typeNodesList.forEach { args += it.toBinary() }
+    this.typeEdgesList.forEach { args += it.toBinary() }
+
+    return args
+  }
+
+  private fun ByteArray.toHex(): String =
+      joinToString(separator = "") { eachByte -> "%02x".format(eachByte) }
+  override fun toString(): String {
+    return generateQueryArgs().joinToString(separator = " - ") { bytes -> bytes.toHex() }
+  }
 }
--- twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/QueryBuffer.kt
+++ twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/QueryBuffer.kt
@@ -1,101 +1,101 @@
-import redis.clients.jedis.Jedis
-import java.lang.Exception
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.twingraph.bulk
+
+import com.cosmotech.api.exceptions.CsmResourceNotFoundException
 import java.nio.charset.Charset
+import redis.clients.jedis.Jedis
 
+@Suppress("MagicNumber", "SpreadOperator")
+class QueryBuffer(
+    val jedis: Jedis,
+    val graphName: String,
+    val maxQuerySize: Int = 512 * 1024 * 1024 /*512 Mo*/
+) {
+  private var first: Boolean = true
 
-class QueryBuffer(val jedis: Jedis, val graphName: String, val max_query_size: Int = 512) {
-    private var first: Boolean = true
+  val tasks: MutableList<BulkQuery> = mutableListOf()
+  // map of redis node creation id to node id
+  private var nodesCreationId = mapOf<String, Long>()
 
-    val tasks:MutableList<BulkQuery> = mutableListOf()
-    // map of redis node creation id to node id
-    private var nodesCreationId = mapOf<String, Long>()
+  private var currentBulkQueryBuilder: BulkQuery.Builder =
+      BulkQuery.Builder().graphName(graphName).first()
+  private var currentType: String? = null
 
-    private var currentBulkQueryBuilder: BulkQuery.Builder = BulkQuery.Builder().graphName(graphName).first()
-    private var currentType: String? = null
+  fun addNodes(type: String, rows: Map<String, Any>) {
+    var addedSize = 0
+    var typeNodes: TypeNodes? = null
 
+    // create a new TypeNodes (for binary header)
+    if (currentType != type) {
+      currentType = type
+      typeNodes = TypeNodes(type, rows.keys.toList())
+      addedSize += typeNodes.size
+    }
+    // create the node
+    val node = Node(rows)
+    addedSize += node.size()
+    nodesCreationId += node.id to nodesCreationId.size.toLong()
 
-    fun addNodes(type: String, rows: Map<String, Any>) {
-        var addedSize = 0
-        var typeNodes: TypeNodes? = null
-
-        // create a new TypeNodes (for binary header)
-        if (currentType != type) {
-            currentType = type
-            typeNodes = TypeNodes(type, rows.keys.toList())
-            addedSize += typeNodes.size
-        }
-        // create the node
-        val node = Node(rows)
-        addedSize += node.size()
-        nodesCreationId += node.id to nodesCreationId.size.toLong()
-
-        // check query size limit
-        if (currentBulkQueryBuilder.size() + addedSize > max_query_size) {
-            tasks.add(currentBulkQueryBuilder.build())
-            currentBulkQueryBuilder = BulkQuery.Builder().graphName(graphName)
-            typeNodes = TypeNodes(type, rows.keys.toList())
-        }
-
-        // add to bulk query builder
-        if (typeNodes != null)
-             currentBulkQueryBuilder.addTypeNode(type, typeNodes)
-        currentBulkQueryBuilder.addNodeToTypeNode(type, node)
+    // check query size limit
+    if (currentBulkQueryBuilder.size() + addedSize > maxQuerySize) {
+      tasks.add(currentBulkQueryBuilder.build())
+      currentBulkQueryBuilder = BulkQuery.Builder().graphName(graphName)
+      typeNodes = TypeNodes(type, rows.keys.toList())
     }
 
-    fun addEdges(type: String, rows: Map<String, Any>) {
-        var addedSize: Int = 0
-        var typeEdges: TypeEdges? = null
+    // add to bulk query builder
+    if (typeNodes != null) currentBulkQueryBuilder.addTypeNode(type, typeNodes)
+    currentBulkQueryBuilder.addNodeToTypeNode(type, node)
+  }
 
-        //extact source and target from row
-        val sourceKey = rows.keys.elementAt(0)
-        val targetKey = rows.keys.elementAt(1)
-        val sourceName = rows[sourceKey]
-        val targetName = rows[targetKey]
+  fun addEdges(type: String, rows: Map<String, Any>) {
+    var addedSize: Int = 0
+    var typeEdges: TypeEdges? = null
 
-        val newRows = rows.minus(sourceKey).minus(targetKey)
+    // extact source and target from row
+    val sourceKey = rows.keys.elementAt(0)
+    val targetKey = rows.keys.elementAt(1)
+    val sourceName = rows[sourceKey]
+    val targetName = rows[targetKey]
 
-        //create a new edge type (for binary header)
-        if (type != currentType) {
-            currentType = type
-            typeEdges = TypeEdges(type, newRows.keys.toList())
-            addedSize += typeEdges.size
-        }
+    val newRows = rows.minus(sourceKey).minus(targetKey)
 
-        //create edge
-        if (sourceName !in nodesCreationId)
-            throw Exception("node $sourceName doesn't exist")
-        if (targetName !in nodesCreationId)
-            throw Exception("node $targetName doesn't exist")
-        var edge = Edge(nodesCreationId[sourceName]!!, nodesCreationId[targetName]!!, newRows)
-        addedSize += edge.size()
-
-        //check query max size
-        if (currentBulkQueryBuilder.size() + addedSize > max_query_size) {
-            tasks.add(currentBulkQueryBuilder.build())
-            currentBulkQueryBuilder = BulkQuery.Builder().graphName(graphName)
-            typeEdges = TypeEdges(type, newRows.keys.toList())
-        }
-
-        //add to bulk query builder
-        if (typeEdges != null)
-            currentBulkQueryBuilder.addTypeEdge(type, typeEdges)
-        currentBulkQueryBuilder.addEdgeToTypeEdge(type, edge)
+    // create a new edge type (for binary header)
+    if (type != currentType) {
+      currentType = type
+      typeEdges = TypeEdges(type, newRows.keys.toList())
+      addedSize += typeEdges.size
     }
 
+    // create edge
+    if (sourceName !in nodesCreationId.entries)
+        throw CsmResourceNotFoundException("Node $sourceName doesn't exist")
+    if (targetName !in nodesCreationId.keys)
+        throw CsmResourceNotFoundException("Node $targetName doesn't exist")
+    var edge = Edge(nodesCreationId[sourceName]!!, nodesCreationId[targetName]!!, newRows)
+    addedSize += edge.size()
 
-    fun send() {
-        tasks.add(currentBulkQueryBuilder.build())
-
-        tasks.forEach { query ->
-
-            println(query.toString())
-            val result = jedis.sendCommand(
-                { "GRAPH.BULK".toByteArray() },
-                *query.generate_query_args()
-            )
-
-            println((result as ByteArray).toString(Charset.defaultCharset()))
-        }
+    // check query max size
+    if (currentBulkQueryBuilder.size() + addedSize > maxQuerySize) {
+      tasks.add(currentBulkQueryBuilder.build())
+      currentBulkQueryBuilder = BulkQuery.Builder().graphName(graphName)
+      typeEdges = TypeEdges(type, newRows.keys.toList())
     }
 
+    // add to bulk query builder
+    if (typeEdges != null) currentBulkQueryBuilder.addTypeEdge(type, typeEdges)
+    currentBulkQueryBuilder.addEdgeToTypeEdge(type, edge)
+  }
+
+  fun send() {
+    tasks.add(currentBulkQueryBuilder.build())
+
+    tasks.forEach { query ->
+      println(query.toString())
+      val result = jedis.sendCommand({ "GRAPH.BULK".toByteArray() }, *query.generateQueryArgs())
+
+      println((result as ByteArray).toString(Charset.defaultCharset()))
+    }
+  }
 }
