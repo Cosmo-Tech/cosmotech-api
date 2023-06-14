8070b0875c33fc60bdd8d278625ab8f33a3487e9
icreated <sergey.polyarus.ext@cosmotech.com>
2023-06-14 15:19:39 +0200

    fix workspace gradle

--- workspace/build.gradle.kts
+++ workspace/build.gradle.kts
@@ -5,7 +5,6 @@ plugins { id("org.jetbrains.kotlinx.kover") }
 dependencies {
   implementation(projects.cosmotechOrganizationApi)
   implementation(projects.cosmotechSolutionApi)
-  implementation(project(mapOf("path" to ":")))
 
   testImplementation("org.testng:testng:7.1.0")
   testImplementation("com.redis.testcontainers:testcontainers-redis-junit:1.6.2")
