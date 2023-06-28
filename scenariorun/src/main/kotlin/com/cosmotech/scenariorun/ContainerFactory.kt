b8d2eb32017b7c34fc0e73ab1fe57f8393d8cf8c
Michel Guillon <michel.guillon@cosmotech.com>
2023-06-28 17:18:07 +0200

    Use scenario's runSizing if provided to run a scenario

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ContainerFactory.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/ContainerFactory.kt
@@ -123,7 +123,7 @@ private const val SCENARIO_DATA_UPLOAD_LOG_LEVEL_ENV_VAR = "CSM_LOG_LEVEL"
 internal const val CSM_JOB_ID_LABEL_KEY = "cosmotech.com/job_id"
 internal const val SCENARIO_DATA_DOWNLOAD_ARTIFACT_NAME = "downloadUrl"
 internal const val WORKFLOW_TYPE_LABEL = "cosmotech.com/workflowtype"
-
+private var useScenarioRunSizing = false
 const val CSM_DAG_ROOT = "DAG_ROOT"
 
 private val LABEL_SIZING =
@@ -149,7 +149,6 @@ class ContainerFactory(
   private val logger = LoggerFactory.getLogger(ContainerFactory::class.java)
 
   private val steps: Map<String, SolutionContainerStepSpec>
-
   init {
     this.steps =
         mapOf(
@@ -519,6 +518,7 @@ class ContainerFactory(
       }
 
       if (testStep(template.run)) {
+        useScenarioRunSizing = scenario.runSizing?.toSizing()?.let { true } ?: false
         containers.addAll(
             buildRunContainersPipeline(
                 currentDependencies,
@@ -1498,8 +1498,8 @@ private fun mergeSolutionContainer(
       }
 
   val runSizing =
-      if (stackedContainer.nodeLabel == NODE_LABEL_DEFAULT &&
-          container.nodeLabel != NODE_LABEL_DEFAULT) {
+      if ((stackedContainer.nodeLabel == NODE_LABEL_DEFAULT &&
+          container.nodeLabel != NODE_LABEL_DEFAULT) || useScenarioRunSizing == true) {
         container.runSizing
       } else {
         stackedContainer.runSizing
