9e6b3eec6b0453590252a0a14b81c4f7144ae8bf
jreynard <jeremy.reynard@cosmotech.com>
2023-06-28 13:27:04 +0200

    Refactor ConnectorServiceIntegrationTest

--- connector/src/integrationTest/kotlin/com/cosmotech/connector/service/ConnectorServiceIntegrationTest.kt
+++ connector/src/integrationTest/kotlin/com/cosmotech/connector/service/ConnectorServiceIntegrationTest.kt
@@ -5,26 +5,29 @@ package com.cosmotech.connector.service
 import com.cosmotech.api.config.CsmPlatformProperties
 import com.cosmotech.api.exceptions.CsmAccessForbiddenException
 import com.cosmotech.api.exceptions.CsmResourceNotFoundException
+import com.cosmotech.api.security.ROLE_ORGANIZATION_USER
 import com.cosmotech.api.security.ROLE_PLATFORM_ADMIN
 import com.cosmotech.api.tests.CsmRedisTestBase
 import com.cosmotech.api.utils.getCurrentAccountIdentifier
 import com.cosmotech.api.utils.getCurrentAuthenticatedRoles
 import com.cosmotech.api.utils.getCurrentAuthenticatedUserName
+import com.cosmotech.api.utils.getCurrentAuthentication
 import com.cosmotech.connector.api.ConnectorApiService
 import com.cosmotech.connector.domain.Connector
 import com.redis.om.spring.RediSearchIndexer
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import io.mockk.mockkStatic
 import kotlin.test.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertNotNull
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.runner.RunWith
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.context.junit4.SpringRunner
@@ -42,79 +45,196 @@ class ConnectorServiceIntegrationTest : CsmRedisTestBase() {
   @Autowired lateinit var connectorApiService: ConnectorApiService
   @Autowired lateinit var csmPlatformProperties: CsmPlatformProperties
 
+  /**
+   * Run once before ALL tests. Should be used to define global setup (static mock, global settings,
+   * ...)
+   */
+  @BeforeAll
+  fun globalSetup() {
+    mockkStatic("com.cosmotech.api.utils.SecurityUtilsKt")
+  }
+
+  /**
+   * Run once before Each tests. Should be used to define volatile configuration (e.g. indexes
+   * creation as db is flush after each test) EXCEPT test context (e.g. no mock for current user)
+   */
   @BeforeEach
   fun setUp() {
-    mockkStatic("com.cosmotech.api.utils.SecurityUtilsKt")
-    every { getCurrentAccountIdentifier(any()) } returns "test.user@cosmotech.com"
-    every { getCurrentAuthenticatedUserName(csmPlatformProperties) } returns "test.user"
-    every { getCurrentAuthenticatedRoles(any()) } returns listOf()
     rediSearchIndexer.createIndexFor(Connector::class.java)
   }
 
-  // TODO Import Connector
+  /** Tests in this nested class are launched as Organization.User */
+  @Nested
+  inner class AsOrganizationUser {
 
-  fun mockConnector(name: String): Connector {
-    return Connector(
-        key = "key",
-        name = name,
-        repository = "repository",
-        version = "version",
-        ioTypes = listOf())
-  }
-
-  @Test
-  fun `test find All Connectors with different pagination params`() {
-    val numberOfConnector = 20
-    logger.info("Creating $numberOfConnector connectors...")
-    IntRange(1, numberOfConnector).forEach {
-      var newConnector = mockConnector("o-connector-test-$it")
-      connectorApiService.registerConnector(newConnector)
+    @BeforeEach
+    fun setUp() {
+      runAsOrganizationUser()
     }
-    var connectorList = connectorApiService.findAllConnectors(null, null)
-    logger.info("Connector list retrieved contains : ${connectorList.size} elements")
-    assertEquals(numberOfConnector, connectorList.size)
-  }
-  @Test
-  fun `test find All Connector - get first page without size `() {
-    val numberOfConnector = 100
-    logger.info("Creating $numberOfConnector organizations...")
-    IntRange(1, numberOfConnector).forEach {
-      var newConnector = mockConnector("o-connector-test-$it")
-      connectorApiService.registerConnector(newConnector)
+
+    @Test
+    fun `register Connector`() {
+      testRegisterConnector()
     }
-    val connectorList = connectorApiService.findAllConnectors(0, null)
-    logger.info("Connector list retrieved contains : ${connectorList.size} elements")
-    assertEquals(csmPlatformProperties.twincache.connector.defaultPageSize, connectorList.size)
-  }
 
-  @Test
-  fun `test find All Connector - get first page with size `() {
-    val numberOfConnector = 100
-    val expectedSize = 75
-    val defaultPageSize = csmPlatformProperties.twincache.connector.defaultPageSize
-    logger.info("Creating $numberOfConnector organizations...")
-    IntRange(1, numberOfConnector).forEach {
-      var newConnector = mockConnector("o-connector-test-$it")
-      connectorApiService.registerConnector(newConnector)
+    @Test
+    fun `findConnectorById`() {
+      testFindConnectorById()
     }
-    var connectorList = connectorApiService.findAllConnectors(0, null)
-    logger.info("Connector list retrieved contains : ${connectorList.size} elements")
-    assertEquals(defaultPageSize, connectorList.size)
 
-    connectorList = connectorApiService.findAllConnectors(0, expectedSize)
-    logger.info("Connector list retrieved contains : ${connectorList.size} elements")
-    assertEquals(expectedSize, connectorList.size)
+    @Test
+    fun `unregister Connector as owner`() {
+      testUnregisterConnectorAsOwner()
+    }
 
-    logger.info("Should return the last page")
-    connectorList = connectorApiService.findAllConnectors(1, expectedSize)
-    assertEquals(numberOfConnector - expectedSize, connectorList.size)
+    @Test
+    fun `unregister Connector as not owner`() {
+      runAsDifferentOrganizationUser()
+      val connectorName = "connectorTest"
+      val connector = createTestConnector(connectorName)
+      logger.info("Create new connector named $connectorName ...")
+      val savedConnector = connectorApiService.registerConnector(connector)
+      runAsOrganizationUser()
+      assertThrows<CsmAccessForbiddenException> {
+        connectorApiService.unregisterConnector(savedConnector.id!!)
+      }
+    }
+
+    @Test
+    fun `find All Connectors with correct values`() {
+      val numberOfConnectorToCreate = 20
+      val defaultPageSize = csmPlatformProperties.twincache.connector.defaultPageSize
+
+      batchConnectorCreation(numberOfConnectorToCreate)
+      testFindAllConnectors(null, null, numberOfConnectorToCreate)
+      testFindAllConnectors(0, null, defaultPageSize)
+      testFindAllConnectors(0, 10, 10)
+      testFindAllConnectors(1, 200, 0)
+      testFindAllConnectors(1, 15, 5)
+    }
+
+    @Test
+    fun `find All Connectors with wrong values`() {
+      testFindAllConnectorsWithWrongValues()
+    }
+
+    @Test
+    fun `import connector`() {
+      testImportConnector()
+    }
+
+    @Test
+    fun `import connector with id null`() {
+      val connector =
+          Connector(
+              key = "key",
+              name = "connectorTest",
+              repository = "repository",
+              version = "version",
+              ioTypes = listOf())
+      assertThrows<CsmResourceNotFoundException> { connectorApiService.importConnector(connector) }
+    }
+
+    /** Run a test with current user as Organization.User */
+    private fun runAsOrganizationUser() {
+      mockkStatic(::getCurrentAuthentication)
+      every { getCurrentAuthentication() } returns mockk<BearerTokenAuthentication>()
+      every { getCurrentAccountIdentifier(any()) } returns "test.user@cosmotech.com"
+      every { getCurrentAuthenticatedUserName(csmPlatformProperties) } returns "test.user"
+      every { getCurrentAuthenticatedRoles(any()) } returns listOf(ROLE_ORGANIZATION_USER)
+    }
   }
 
-  @Test
-  fun `test find All Connector with wrong pagination params`() {
-    var newConnector = mockConnector("o-connector-test-1")
-    connectorApiService.registerConnector(newConnector)
+  /** Tests in this nested class are launched as Platform.Admin */
+  @Nested
+  inner class AsPlatformAdmin {
 
+    @BeforeEach
+    fun setUp() {
+      runAsPlatformAdmin()
+    }
+
+    @Test
+    fun `register Connector`() {
+      testRegisterConnector()
+    }
+
+    @Test
+    fun `findConnectorById`() {
+      testFindConnectorById()
+    }
+
+    @Test
+    fun `unregister Connector as owner`() {
+      testUnregisterConnectorAsOwner()
+    }
+
+    @Test
+    fun `unregister Connector as not owner`() {
+      runAsDifferentOrganizationUser()
+      val connectorName = "connectorTest"
+      val connector = createTestConnector(connectorName)
+      logger.info("Create new connector named $connectorName ...")
+      val savedConnector = connectorApiService.registerConnector(connector)
+      runAsPlatformAdmin()
+      connectorApiService.unregisterConnector(savedConnector.id!!)
+      assertThrows<CsmResourceNotFoundException> {
+        connectorApiService.findConnectorById(savedConnector.id!!)
+      }
+    }
+
+    @Test
+    fun `find All Connectors with correct values`() {
+      val numberOfConnectorToCreate = 20
+      val defaultPageSize = csmPlatformProperties.twincache.connector.defaultPageSize
+
+      batchConnectorCreation(numberOfConnectorToCreate)
+      testFindAllConnectors(null, null, numberOfConnectorToCreate)
+      testFindAllConnectors(0, null, defaultPageSize)
+      testFindAllConnectors(0, 10, 10)
+      testFindAllConnectors(1, 200, 0)
+      testFindAllConnectors(1, 15, 5)
+    }
+
+    @Test
+    fun `find All Connectors with wrong values`() {
+      testFindAllConnectorsWithWrongValues()
+    }
+
+    @Test
+    fun `import connector`() {
+      testImportConnector()
+    }
+
+    @Test
+    fun `import connector with id null`() {
+      val connector =
+          Connector(
+              key = "key",
+              name = "connectorTest",
+              repository = "repository",
+              version = "version",
+              ioTypes = listOf())
+      assertThrows<CsmResourceNotFoundException> { connectorApiService.importConnector(connector) }
+    }
+
+    /** Run a test with current user as Platform.Admin */
+    private fun runAsPlatformAdmin() {
+      mockkStatic(::getCurrentAuthentication)
+      every { getCurrentAuthentication() } returns mockk<BearerTokenAuthentication>()
+      every { getCurrentAccountIdentifier(any()) } returns "test.admin@cosmotech.com"
+      every { getCurrentAuthenticatedUserName(csmPlatformProperties) } returns "test.admin"
+      every { getCurrentAuthenticatedRoles(any()) } returns listOf(ROLE_PLATFORM_ADMIN)
+    }
+  }
+
+  internal fun testFindAllConnectors(page: Int?, size: Int?, expectedResultSize: Int) {
+    val connectorList = connectorApiService.findAllConnectors(page, size)
+    logger.info("Connector list retrieved contains : ${connectorList.size} elements")
+    assertEquals(expectedResultSize, connectorList.size)
+  }
+
+  internal fun testFindAllConnectorsWithWrongValues() {
     logger.info("Should throw IllegalArgumentException when page and size are zeros")
     assertThrows<IllegalArgumentException> { connectorApiService.findAllConnectors(0, 0) }
 
@@ -125,51 +245,74 @@ class ConnectorServiceIntegrationTest : CsmRedisTestBase() {
     assertThrows<IllegalArgumentException> { connectorApiService.findAllConnectors(0, -1) }
   }
 
-  @Test
-  fun registerConnector() {
-    val connector1 = mockConnector("connector1")
-    val connector2 = mockConnector("connector2")
-
-    logger.info("Create new connector...")
-    val connectorRegistered1 = connectorApiService.registerConnector(connector1)
-    connectorApiService.registerConnector(connector2)
-
-    logger.info("Fetch new connector created ...")
-    val connectorRetrieved = connectorApiService.findConnectorById(connectorRegistered1.id!!)
-    assertEquals(connectorRegistered1, connectorRetrieved)
-
-    logger.info("Deleting connector ...")
-    connectorApiService.unregisterConnector(connectorRegistered1.id!!)
-    assertThrows<CsmResourceNotFoundException> {
-      connectorApiService.findConnectorById(connectorRegistered1.id!!)
-    }
-  }
-  @Test
-  fun `can delete connector when user is not the owner and is Platform Admin`() {
-    val connector1 = mockConnector("connector1")
-    logger.info("Create new connector...")
-    val connectorRegistered1 = connectorApiService.registerConnector(connector1)
-    logger.info("Change current user...")
-    every { getCurrentAccountIdentifier(any()) } returns "test.admin@cosmotech.com"
-    every { getCurrentAuthenticatedUserName(csmPlatformProperties) } returns "test.admin"
-    every { getCurrentAuthenticatedRoles(any()) } returns listOf(ROLE_PLATFORM_ADMIN)
-    connectorApiService.unregisterConnector(connectorRegistered1.id!!)
-    assertThrows<CsmResourceNotFoundException> {
-      connectorApiService.findConnectorById(connectorRegistered1.id!!)
+  internal fun testRegisterConnector() {
+    val connectorName = "connectorTest"
+    val connector = createTestConnector(connectorName)
+    logger.info("Create new connector named $connectorName ...")
+    assertDoesNotThrow {
+      val savedConnector = connectorApiService.registerConnector(connector)
+      assertNotNull(savedConnector)
     }
   }
 
-  @Test
-  fun `cannot delete connector when user is not the owner and is not Platform Admin`() {
-    val connector1 = mockConnector("connector1")
-    logger.info("Create new connector...")
-    val connectorRegistered1 = connectorApiService.registerConnector(connector1)
-    logger.info("Change current user...")
+  internal fun testFindConnectorById() {
+    val connectorName = "connectorTest"
+    val connector = createTestConnector(connectorName)
+    logger.info("Create new connector named $connectorName ...")
+    assertDoesNotThrow {
+      val savedConnector = connectorApiService.registerConnector(connector)
+      val retrievedConnector = connectorApiService.findConnectorById(savedConnector.id!!)
+      assertEquals(savedConnector, retrievedConnector)
+    }
+  }
+
+  internal fun testUnregisterConnectorAsOwner() {
+    val connectorName = "connectorTest"
+    val connector = createTestConnector(connectorName)
+    logger.info("Create new connector named $connectorName ...")
+    assertDoesNotThrow {
+      val savedConnector = connectorApiService.registerConnector(connector)
+      connectorApiService.unregisterConnector(savedConnector.id!!)
+      assertThrows<CsmResourceNotFoundException> {
+        connectorApiService.findConnectorById(savedConnector.id!!)
+      }
+    }
+  }
+
+  internal fun testImportConnector() {
+    val connectorName = "connectorTest"
+    val connector = createTestConnector(connectorName)
+    val importedConnector = connectorApiService.importConnector(connector)
+    assertDoesNotThrow {
+      connectorApiService.findConnectorById(connector.id!!)
+      assertEquals(connector, importedConnector)
+    }
+  }
+
+  /** Run a test with different Organization.User */
+  internal fun runAsDifferentOrganizationUser() {
     every { getCurrentAccountIdentifier(any()) } returns "test.other.user@cosmotech.com"
     every { getCurrentAuthenticatedUserName(csmPlatformProperties) } returns "test.other.user"
-    every { getCurrentAuthenticatedRoles(any()) } returns listOf()
-    assertThrows<CsmAccessForbiddenException> {
-      connectorApiService.unregisterConnector(connectorRegistered1.id!!)
+    every { getCurrentAuthenticatedRoles(any()) } returns listOf(ROLE_ORGANIZATION_USER)
+  }
+
+  /** Create default test Connector */
+  internal fun createTestConnector(name: String): Connector {
+    return Connector(
+        id = "connector_id",
+        key = "key",
+        name = name,
+        repository = "repository",
+        version = "version",
+        ioTypes = listOf())
+  }
+
+  /** Connector batch creation */
+  internal fun batchConnectorCreation(numberOfConnectorToCreate: Int) {
+    logger.info("Creating $numberOfConnectorToCreate connectors...")
+    IntRange(1, numberOfConnectorToCreate).forEach {
+      val newConnector = createTestConnector("o-connector-test-$it")
+      connectorApiService.registerConnector(newConnector)
     }
   }
 }
