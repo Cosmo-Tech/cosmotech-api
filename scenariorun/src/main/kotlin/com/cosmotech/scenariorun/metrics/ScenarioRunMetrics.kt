c655fd76939b3a3c5f56d3eba761cb19e7d04845
jreynard <jeremy.reynard@cosmotech.com>
2023-07-19 19:02:42 +0200

    Rework ScenarioRunMetrics

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/metrics/ScenarioRunMetrics.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/metrics/ScenarioRunMetrics.kt
@@ -21,6 +21,14 @@ private const val RUNNING_STATUS = "Running"
 
 private const val SERVICE_NAME = "scenariorun"
 
+private const val ORGANIZATION_ID_LABEL = "organizationId"
+private const val WORKSPACE_ID_LABEL = "workspaceId"
+private const val SCENARIO_ID_LABEL = "scenarioId"
+
+private const val DEFAULT_EMPTY_WORKFLOW_LABEL = "none"
+private const val TOTAL_QUALIFIER = "total"
+private const val TS_RUNNING_WORKFLOW_NAME = "running"
+private const val TS_RUN_WORKFLOW_NAME = "run"
 
 @Service
 internal class ScenarioRunMetrics(
@@ -28,118 +36,126 @@ internal class ScenarioRunMetrics(
     private val eventPublisher: CsmEventPublisher,
     private val csmPlatformProperties: CsmPlatformProperties,
 ) {
-  private fun getRunningWorkflowsCount(): Map<WorkflowContextData?, Int> {
-    val runWorkflows = this.workflowService.findWorkflowStatusByLabel(
-        "$WORKFLOW_TYPE_LABEL=$WORKFLOW_TYPE_SCENARIO_RUN",
-        true,
-    )
-    return runWorkflows.filter { it.status == RUNNING_STATUS }.groupingBy { it.contextData }.eachCount()
-  }
-
-  private fun publishRunningLabeledMetric(value: Int, name: String, labels: Map<String, String>) {
-    val orgaMetric = PersistentMetric(
-        service = SERVICE_NAME,
-        name = name,
-        value = value.toDouble(),
-        qualifier = "total",
-        labels = labels,
-        type = PersitentMetricType.GAUGE,
-        downSampling = true,
-        downSamplingAggregation = DownSamplingAggregationType.SUM,
-    )
-    eventPublisher.publishEvent(PersistentMetricEvent(this, orgaMetric))
-  }
 
   @Scheduled(fixedDelay = 10000)
   fun publishCurrentRunningScenario() {
     if (!csmPlatformProperties.metrics.enabled) return
+
     val countRunningWorkflows = getRunningWorkflowsCount()
-    var totalRunningWorklowsCount = 0
-    countRunningWorkflows.forEach { (workflowContext, workflowCount) ->
-      totalRunningWorklowsCount += workflowCount
-      if (workflowContext!!.organizationId != "none") {
-        publishRunningLabeledMetric(
-            workflowCount,
-            "running:" + workflowContext.organizationId,
-            mapOf(
-                "usage" to "licensing",
-                "organizationId" to workflowContext.organizationId!!,
-            ))
-        if (workflowContext.workspaceId != "none") {
-          publishRunningLabeledMetric(
-              workflowCount,
-              "running:" + workflowContext.organizationId + ":" + workflowContext.workspaceId,
-              mapOf(
-                  "usage" to "licensing",
-                  "organizationId" to workflowContext.organizationId,
-                  "workspaceId" to workflowContext.workspaceId!!,
-              ))
-        }
-      }
-    }
-    val metric = PersistentMetric(
-        service = SERVICE_NAME,
-        name = "running",
-        value = totalRunningWorklowsCount.toDouble(),
-        qualifier = "total",
-        labels = mapOf(
+    val labels =
+        mutableMapOf(
             "usage" to "licensing",
-        ),
-        type = PersitentMetricType.GAUGE,
-        downSampling = true,
-        downSamplingAggregation = DownSamplingAggregationType.MAX,
-    )
-    eventPublisher.publishEvent(PersistentMetricEvent(this, metric))
+        )
+    // Global
+    publishRunningLabeledMetric(
+        countRunningWorkflows.values.sum(), TS_RUNNING_WORKFLOW_NAME, labels)
+
+    // By Organization, Workspace, Scenario
+    countRunningWorkflows.forEach { (workflowContext, workflowCount) ->
+      publishOrganizationWorkspaceAndScenarioRunningLabeledMetricIfAny(
+          workflowContext!!, workflowCount, TS_RUNNING_WORKFLOW_NAME, labels)
+    }
   }
 
   @EventListener(ScenarioRunStartedForScenario::class)
-  @Suppress("UnusedPrivateMember")
   fun onScenarioRunStartedForScenario(event: ScenarioRunStartedForScenario) {
     if (!csmPlatformProperties.metrics.enabled) return
-    val metric = PersistentMetric(
-        service = SERVICE_NAME,
-        name = "run",
-        value = 0.0,
-        incrementBy = 1,
-        qualifier = "total",
-        labels = mapOf(
+
+    val labels =
+        mutableMapOf(
             "usage" to "licensing",
-        ),
-        type = PersitentMetricType.COUNTER,
-        downSampling = true,
-        downSamplingAggregation = DownSamplingAggregationType.MAX,
-    )
+        )
+    var name = TS_RUN_WORKFLOW_NAME
+
+    // Global
+    publishRunStartLabeledMetric(name, labels)
+
+    // By Organization
+    val organizationId = event.organizationId
+    labels[ORGANIZATION_ID_LABEL] = organizationId
+    name += ":$organizationId"
+    publishRunStartLabeledMetric(name, labels)
+
+    // By Workspace
+    val workspaceId = event.workspaceId
+    labels[WORKSPACE_ID_LABEL] = workspaceId
+    name += ":$workspaceId"
+    publishRunStartLabeledMetric(name, labels)
+
+    // By Scenario
+    val scenarioId = event.scenarioId
+    labels[SCENARIO_ID_LABEL] = scenarioId
+    name += ":$scenarioId"
+    publishRunStartLabeledMetric(name, labels)
+  }
+
+  private fun getRunningWorkflowsCount(): Map<WorkflowContextData?, Int> {
+    val runWorkflows =
+        this.workflowService.findWorkflowStatusByLabel(
+            "$WORKFLOW_TYPE_LABEL=$WORKFLOW_TYPE_SCENARIO_RUN",
+            true,
+        )
+    return runWorkflows
+        .filter { it.status == RUNNING_STATUS }
+        .groupingBy { it.contextData }
+        .eachCount()
+  }
+
+  internal fun publishRunStartLabeledMetric(name: String, labels: Map<String, String>) {
+    val metric =
+        PersistentMetric(
+            service = SERVICE_NAME,
+            name = name,
+            value = 0.0,
+            incrementBy = 1,
+            qualifier = TOTAL_QUALIFIER,
+            labels = labels,
+            type = PersitentMetricType.COUNTER,
+            downSampling = true,
+            downSamplingAggregation = DownSamplingAggregationType.MAX,
+        )
     eventPublisher.publishEvent(PersistentMetricEvent(this, metric))
-    val organizationMetric = PersistentMetric(
-        service = SERVICE_NAME,
-        name = "run:" + event.organizationId,
-        value = 0.0,
-        incrementBy = 1,
-        qualifier = "total",
-        labels = mapOf(
-            "usage" to "licensing",
-            "organizationId" to event.organizationId,
-        ),
-        type = PersitentMetricType.COUNTER,
-        downSampling = true,
-        downSamplingAggregation = DownSamplingAggregationType.MAX,
-    )
-    eventPublisher.publishEvent(PersistentMetricEvent(this, organizationMetric))
-    val workspaceMetric = PersistentMetric(
-        service = SERVICE_NAME,
-        name = "run:" + event.organizationId + ":" + event.workspaceId,
-        value = 0.0,
-        incrementBy = 1,
-        qualifier = "total",
-        labels = mapOf(
-            "usage" to "licensing",
-            "organizationId" to event.organizationId,
-            "workspaceId" to event.workspaceId,
-        ),
-        type = PersitentMetricType.COUNTER,
-        downSampling = true,
-        downSamplingAggregation = DownSamplingAggregationType.MAX,
-    )
-    eventPublisher.publishEvent(PersistentMetricEvent(this, workspaceMetric))
+  }
+
+  internal fun publishOrganizationWorkspaceAndScenarioRunningLabeledMetricIfAny(
+      workflowContext: WorkflowContextData,
+      workflowCount: Int,
+      name: String,
+      labels: MutableMap<String, String>
+  ) {
+    var existingName = name
+    val organizationId = workflowContext.organizationId!!
+    if (organizationId != DEFAULT_EMPTY_WORKFLOW_LABEL) {
+      labels[ORGANIZATION_ID_LABEL] = organizationId
+      existingName += ":$organizationId"
+      publishRunningLabeledMetric(workflowCount, existingName, labels)
+      val workspaceId = workflowContext.workspaceId!!
+      if (workspaceId != DEFAULT_EMPTY_WORKFLOW_LABEL) {
+        labels[WORKSPACE_ID_LABEL] = workspaceId
+        existingName += ":$workspaceId"
+        publishRunningLabeledMetric(workflowCount, existingName, labels)
+        val scenarioId = workflowContext.scenarioId!!
+        if (scenarioId != DEFAULT_EMPTY_WORKFLOW_LABEL) {
+          labels[SCENARIO_ID_LABEL] = scenarioId
+          existingName += ":$scenarioId"
+          publishRunningLabeledMetric(workflowCount, existingName, labels)
+        }
+      }
+    }
+  }
+
+  internal fun publishRunningLabeledMetric(value: Int, name: String, labels: Map<String, String>) {
+    val metric =
+        PersistentMetric(
+            service = SERVICE_NAME,
+            name = name,
+            value = value.toDouble(),
+            qualifier = TOTAL_QUALIFIER,
+            labels = labels,
+            type = PersitentMetricType.GAUGE,
+            downSampling = true,
+            downSamplingAggregation = DownSamplingAggregationType.SUM,
+        )
+    eventPublisher.publishEvent(PersistentMetricEvent(this, metric))
   }
 }
