583d7c8d23b3ea7289fa06d21ecab2fdf759b723
Leopold Cramer <leopold.h.cramer@gmail.com>
2023-07-31 17:40:26 +0200

    add check for source/target/id name and case

--- twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/model/Node.kt
+++ twingraph/src/main/kotlin/com/cosmotech/twingraph/bulk/model/Node.kt
@@ -2,13 +2,11 @@
 // Licensed under the MIT license.
 package com.cosmotech.twingraph.bulk.model
 
-import com.cosmotech.api.exceptions.CsmResourceNotFoundException
 import com.cosmotech.twingraph.bulk.getNodeBinaryBlobFormat
 
 class Node(override val properties: Map<String, String>) : AbstractEntity() {
 
-  val id: String =
-      properties["id"] ?: throw CsmResourceNotFoundException("Node property 'id' not found")
+  val id: String = properties["id"]!!
 
   override fun toBinaryFormat(): ByteArray {
     return getNodeBinaryBlobFormat(getPropertiesBinary())
