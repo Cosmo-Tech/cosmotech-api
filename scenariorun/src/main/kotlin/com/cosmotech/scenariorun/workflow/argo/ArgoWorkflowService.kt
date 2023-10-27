dbde4484a0c2cab5a0528fa1cdda4ca040acdd27
Michel Guillon <michel.guillon@cosmotech.com>
2023-10-27 14:53:49 +0200

    Hack due to Argo workflows not stopping Force the scenario, scenariorun state to failed

--- scenariorun/src/main/kotlin/com/cosmotech/scenariorun/workflow/argo/ArgoWorkflowService.kt
+++ scenariorun/src/main/kotlin/com/cosmotech/scenariorun/workflow/argo/ArgoWorkflowService.kt
@@ -12,6 +12,7 @@ import com.cosmotech.scenariorun.domain.ScenarioRunContainerLogs
 import com.cosmotech.scenariorun.domain.ScenarioRunLogs
 import com.cosmotech.scenariorun.domain.ScenarioRunResourceRequested
 import com.cosmotech.scenariorun.domain.ScenarioRunStartContainers
+import com.cosmotech.scenariorun.domain.ScenarioRunState
 import com.cosmotech.scenariorun.domain.ScenarioRunStatus
 import com.cosmotech.scenariorun.domain.ScenarioRunStatusNode
 import com.cosmotech.scenariorun.workflow.WorkflowContextData
@@ -437,7 +438,8 @@ internal class ArgoWorkflowService(
         workflowName = scenarioRun.workflowName,
         startTime = workflowStatus?.startedAt?.toString(),
         endTime = workflowStatus?.finishedAt?.toString(),
-        phase = workflowStatus?.phase,
+        phase =
+            if (scenarioRun.state == ScenarioRunState.Failed) "Failed" else workflowStatus?.phase,
         progress = workflowStatus?.progress,
         message = workflowStatus?.message,
         estimatedDuration = workflowStatus?.estimatedDuration,
