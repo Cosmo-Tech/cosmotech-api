a453578e6569c3e84b3b3f37c931590835809aa4
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2023-11-28 16:56:27 +0100

    Fix Twingraph tests

--- twingraph/src/test/kotlin/com/cosmotech/twingraph/utils/TwingraphExtTests.kt
+++ twingraph/src/test/kotlin/com/cosmotech/twingraph/utils/TwingraphExtTests.kt
@@ -2,16 +2,29 @@
 // Licensed under the MIT license.
 package com.cosmotech.twingraph.utils
 
+import com.cosmotech.twingraph.extension.toJsonString
+import io.mockk.every
+import io.mockk.mockk
+import kotlin.test.Test
+import redis.clients.jedis.graph.ResultSet
+
 class TwingraphExtTests {
-  /*  @Test
+
+  @Test
   fun `ResultSet to json on null values`() {
-    val resultSet = spyk<ResultSet>()
-    val rec1 = RecordImpl(listOf("name", "value1", "value2"), listOf("a", "b", null))
-    val rec2 = RecordImpl(listOf("name", "value1", "value2"), listOf("c", null, "d"))
-    every { resultSet.iterator() } returns mutableListOf(rec1, rec2).iterator()
+    val resultSet = mockk<ResultSet>()
+    val record1 = mockk<redis.clients.jedis.graph.Record>()
+    val record2 = mockk<redis.clients.jedis.graph.Record>()
+
+    every { resultSet.iterator() } returns arrayListOf(record1, record2).listIterator()
+    every { record1.keys() } returns listOf("name", "value1", "value2")
+    every { record1.values() } returns listOf("a", "b", null)
+
+    every { record2.keys() } returns listOf("name", "value1", "value2")
+    every { record2.values() } returns listOf("c", null, "d")
 
     val jsonResult = resultSet.toJsonString()
 
     assert(jsonResult == "[{\"name\":\"a\",\"value1\":\"b\"},{\"name\":\"c\",\"value2\":\"d\"}]")
-  }*/
+  }
 }
