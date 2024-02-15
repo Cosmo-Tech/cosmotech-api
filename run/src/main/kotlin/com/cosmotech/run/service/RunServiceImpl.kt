d385f23bbee48b3fae2ee79d3be3756438f63f6e
Laurent Alépée <laurent.alepee@cosmotech.com>
2024-02-15 10:31:27 +0100

    create runService Bean for build compliance

---
+++ run/src/main/kotlin/com/cosmotech/run/service/RunServiceImpl.kt
@@ -0,0 +1,58 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.run.service
+
+import com.cosmotech.run.api.RunApiService
+import com.cosmotech.run.domain.Run
+import com.cosmotech.run.domain.RunLogs
+import com.cosmotech.run.domain.RunStatus
+import org.springframework.stereotype.Service
+
+@Service
+class RunServiceImpl : RunApiService {
+  override fun deleteRun(
+      organizationId: String,
+      workspaceId: String,
+      runnerId: String,
+      runId: String
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  override fun findRunById(
+      organizationId: String,
+      workspaceId: String,
+      runnerId: String,
+      runId: String
+  ): Run {
+    TODO("Not yet implemented")
+  }
+
+  override fun getRunLogs(
+      organizationId: String,
+      workspaceId: String,
+      runnerId: String,
+      runId: String
+  ): RunLogs {
+    TODO("Not yet implemented")
+  }
+
+  override fun getRunStatus(
+      organizationId: String,
+      workspaceId: String,
+      runnerId: String,
+      runId: String
+  ): RunStatus {
+    TODO("Not yet implemented")
+  }
+
+  override fun getRuns(
+      organizationId: String,
+      workspaceId: String,
+      runnerId: String,
+      page: Int?,
+      size: Int?
+  ): List<Run> {
+    TODO("Not yet implemented")
+  }
+}
