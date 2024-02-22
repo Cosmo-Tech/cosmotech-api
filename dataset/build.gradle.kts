24f3d5300c7bfd3cbe43648279b3a9985b72a86d
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-02-22 16:33:33 +0100

    Upgrade org.springframework.boot:spring-boot-starter-test dep from 3.1.5 to 3.2.2 in modules

--- dataset/build.gradle.kts
+++ dataset/build.gradle.kts
@@ -9,11 +9,7 @@ dependencies {
   implementation("org.apache.commons:commons-compress:1.24.0")
   testImplementation("org.testng:testng:7.8.0")
   testImplementation("com.redis.testcontainers:testcontainers-redis-junit:1.6.4")
-  testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5")
-
-  implementation("com.redislabs:jredisgraph:2.5.1") {
-    constraints { implementation("org.apache.commons:commons-text:1.10.0") }
-  }
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.2")
 
   testImplementation(projects.cosmotechSolutionApi)
   testImplementation(projects.cosmotechWorkspaceApi)
