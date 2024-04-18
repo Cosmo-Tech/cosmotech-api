637982a5d01f78bdccd39148910a377ccec8d3e4
Alexis Fossart <alexis.fossart@cosmotech.com>
2024-04-18 10:40:12 +0200

    Add integration test for sendRunData

--- run/src/integrationTest/kotlin/com/cosmotech/run/service/CsmPostgresTestBase.kt
+++ run/src/integrationTest/kotlin/com/cosmotech/run/service/CsmPostgresTestBase.kt
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 package com.cosmotech.run.service
 
+import com.cosmotech.api.tests.CsmRedisTestBase
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
@@ -14,11 +15,12 @@ import org.testcontainers.utility.MountableFile
 
 @Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-open class CsmPostgresTestBase {
+open class CsmPostgresTestBase: CsmRedisTestBase() {
 
   companion object {
     private const val ADMIN_USER_CREDENTIALS = "adminusertest"
     private const val READER_USER_CREDENTIALS = "readusertest"
+    private const val WRITER_USER_CREDENTIALS = "writeusertest"
 
     var postgres: PostgreSQLContainer<*> =
         PostgreSQLContainer("postgres:alpine3.19")
@@ -36,6 +38,8 @@ open class CsmPostgresTestBase {
       registry.add("csm.platform.storage.port") { postgres.getMappedPort(POSTGRESQL_PORT) }
       registry.add("csm.platform.storage.admin.username") { ADMIN_USER_CREDENTIALS }
       registry.add("csm.platform.storage.admin.password") { ADMIN_USER_CREDENTIALS }
+      registry.add("csm.platform.storage.writer.username") { WRITER_USER_CREDENTIALS }
+      registry.add("csm.platform.storage.writer.password") { WRITER_USER_CREDENTIALS }
       registry.add("csm.platform.storage.reader.username") { READER_USER_CREDENTIALS }
       registry.add("csm.platform.storage.reader.password") { READER_USER_CREDENTIALS }
     }
