68eed502638b5554a3b0f1def6903c68fac8dac9
Leopold Cramer <leopold.h.cramer@gmail.com>
2024-04-22 17:41:10 +0200

    WIP

--- run/src/main/kotlin/com/cosmotech/run/service/RunServiceImpl.kt
+++ run/src/main/kotlin/com/cosmotech/run/service/RunServiceImpl.kt
@@ -11,6 +11,7 @@ import com.cosmotech.api.rbac.PERMISSION_READ
 import com.cosmotech.api.rbac.PERMISSION_WRITE
 import com.cosmotech.api.rbac.getScenarioRolesDefinition
 import com.cosmotech.api.utils.constructPageRequest
+import com.cosmotech.dataset.bulk.toPropertyType
 import com.cosmotech.run.RunApiServiceInterface
 import com.cosmotech.run.RunContainerFactory
 import com.cosmotech.run.config.createDB
@@ -28,6 +29,7 @@ import com.cosmotech.run.domain.RunStatus
 import com.cosmotech.run.domain.RunTemplateParameterValue
 import com.cosmotech.run.domain.SendRunDataRequest
 import com.cosmotech.run.repository.RunRepository
+import com.cosmotech.run.service.RunServiceImpl.KotlinxGenericMapSerializer.toJsonElement
 import com.cosmotech.run.utils.isTerminal
 import com.cosmotech.run.utils.withoutSensitiveData
 import com.cosmotech.run.workflow.WorkflowService
@@ -38,7 +40,23 @@ import com.google.gson.Gson
 import java.sql.ResultSetMetaData
 import java.sql.SQLException
 import java.time.Instant
-import org.json.JSONObject
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.doubleOrNull
+import kotlinx.serialization.json.longOrNull
+import kotlinx.serialization.serializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.event.EventListener
 import org.springframework.data.domain.PageRequest
@@ -281,6 +299,68 @@ class RunServiceImpl(
     return this.sendDataToStorage(runId, sendRunDataRequest.id!!, sendRunDataRequest.data)
   }
 
+  object KotlinxGenericMapSerializer : KSerializer<Map<String, Any?>> {
+
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("GenericMap")
+
+    override fun serialize(encoder: Encoder, value: Map<String, Any?>) {
+      val jsonObject = JsonObject(value.mapValues { it.value.toJsonElement() })
+      val jsonObjectSerializer = encoder.serializersModule.serializer<JsonObject>()
+      jsonObjectSerializer.serialize(encoder, jsonObject)
+    }
+
+    override fun deserialize(decoder: Decoder): Map<String, Any?> {
+      val jsonDecoder =
+          decoder as? JsonDecoder
+              ?: throw SerializationException("Can only deserialize Json content to generic Map")
+      val root = jsonDecoder.decodeJsonElement()
+      return if (root is JsonObject) root.toMap()
+      else throw SerializationException("Cannot deserialize Json content to generic Map")
+    }
+
+    fun Any?.toJsonElement(): JsonElement =
+        when (this) {
+          null -> JsonNull
+          is String -> JsonPrimitive(this)
+          is Number -> JsonPrimitive(this)
+          is Boolean -> JsonPrimitive(this)
+          is Map<*, *> -> toJsonObject()
+          is Iterable<*> -> toJsonArray()
+          else -> throw SerializationException("Cannot serialize value type $this")
+        }
+
+    private fun Map<*, *>.toJsonObject(): JsonObject =
+        JsonObject(this.entries.associate { it.key.toString() to it.value.toJsonElement() })
+
+    private fun Iterable<*>.toJsonArray(): JsonArray = JsonArray(this.map { it.toJsonElement() })
+
+    private fun JsonElement.toAnyNullableValue(): Any? =
+        when (this) {
+          is JsonPrimitive -> toScalarOrNull()
+          is JsonObject -> toMap()
+          is JsonArray -> toList()
+        }
+
+    private fun JsonObject.toMap(): Map<String, Any?> =
+        entries.associate {
+          when (val jsonElement = it.value) {
+            is JsonPrimitive -> it.key to jsonElement.toScalarOrNull()
+            is JsonObject -> it.key to jsonElement.toMap()
+            is JsonArray -> it.key to jsonElement.toAnyNullableValueList()
+          }
+        }
+
+    private fun JsonPrimitive.toScalarOrNull(): Any? =
+        when {
+          this is JsonNull -> null
+          this.isString -> this.content
+          else -> listOfNotNull(booleanOrNull, longOrNull, doubleOrNull).firstOrNull()
+        }
+
+    private fun JsonArray.toAnyNullableValueList(): List<Any?> =
+        this.map { it.toAnyNullableValue() }
+  }
+
   override fun queryRunData(
       organizationId: String,
       workspaceId: String,
@@ -303,22 +383,30 @@ class RunServiceImpl(
 
     val md: ResultSetMetaData = queryResults.metaData
     val columns = md.columnCount
-    val rows: MutableList<Map<String, Any>> = ArrayList()
+    //    val rows: MutableList<Map<String, Any>> = ArrayList()
+    val listjson = emptyList<JsonObject>().toMutableList()
     while (queryResults.next()) {
       val row: MutableMap<String, Any> = HashMap(columns)
       for (i in 1..columns) {
         row[md.getColumnLabel(i)] = queryResults.getObject(i)
       }
-      rows.add(row)
-    }
-    val resultList = emptyList<JSONObject>().toMutableList()
-    rows.forEach {
-      val json = JSONObject()
-      it.forEach { element -> json.put(element.key, element.value.toString()) }
-      resultList.add(json)
-    }
+      //      KotlinxGenericMapSerializer.serialize()
+      //      val ar = JSONArray(row)
+      val obj = buildJsonObject {
+        for (i in 1..md.columnCount) {
+          println(queryResults.getObject(i))
+          put(md.getColumnLabel(i), queryResults.getObject(i).toPropertyType().toJsonElement())
+        }
+      }
+      listjson.add(obj)
 
-    return QueryResult(resultList)
+      //      listjson.add(ar)
+      //      rows.add(row)
+      //      println(rows)
+    }
+    //    val array = JsonArray(listjson.toList())
+
+    return QueryResult(listjson)
   }
 
   private fun Run.withStateInformation(): Run {
