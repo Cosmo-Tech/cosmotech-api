a3a1b64e203ed13bb52ba7206fd02e22b17a0e5f
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-08-27 14:26:20 +0200

    Add csm.platform.internalResultServices.eventBus.enabled property as ConditionalOnExpression for AMQP beans + fix integration tests

--- run/src/main/kotlin/com/cosmotech/run/config/RabbitMqConfigModel.kt
+++ run/src/main/kotlin/com/cosmotech/run/config/RabbitMqConfigModel.kt
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnExpression("'\${csm.platform.internalResultServices.enabled}' == 'true'")
+@ConditionalOnExpression(
+    "'\${csm.platform.internalResultServices.enabled}' == 'true' " +
+        "and '\${csm.platform.internalResultServices.eventBus.enabled}' == 'true'")
 class RabbitMqConfigModel {
 
   @Value("\${csm.platform.internalResultServices.eventBus.default-exchange}")
