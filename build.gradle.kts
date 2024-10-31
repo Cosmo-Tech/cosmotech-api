6420a3461f1690b36f4d6908579806c0560dddb0
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2024-10-31 11:26:34 +0100

    Upgrade cosmotech-api-common version from 1.0.4 to 1.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ val springWebVersion = "6.1.4"
 
 // Implementation
 val kotlinJvmTarget = 21
-val cosmotechApiCommonVersion = "1.0.4"
+val cosmotechApiCommonVersion = "1.0.5"
 val cosmotechApiAzureVersion = "1.0.1"
 val jedisVersion = "4.4.6"
 val springOauthVersion = "6.2.2"
