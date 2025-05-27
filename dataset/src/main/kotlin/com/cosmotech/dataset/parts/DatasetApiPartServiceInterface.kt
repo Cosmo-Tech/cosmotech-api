ef73acebcac9b5b25916abda43d6b52c99f2a7b0
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2025-05-27 11:35:37 +0200

    Add DatasetPart service and enhance Dataset model/schema
    Introduce `DatasetApiPartServiceInterface` and `DatasetPartServiceImpl` with placeholder methods for dataset part operations. Extend Dataset model and OpenAPI schema to include `organizationId`, `workspaceId`, and `datasetId` fields, ensuring better association and indexing capabilities.

---
+++ dataset/src/main/kotlin/com/cosmotech/dataset/parts/DatasetApiPartServiceInterface.kt
@@ -0,0 +1,7 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.dataset.parts
+
+import com.cosmotech.dataset.api.DatasetPartApiService
+
+@Suppress("EmptyClassBlock") interface DatasetApiPartServiceInterface : DatasetPartApiService {}
