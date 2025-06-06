00a072932b73b41d5c4886ac4d71d494f6049fea
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2025-06-06 15:43:41 +0200

    Enhance Dataset integration tests to validate dataset part creation and include file upload testing

--- api/src/integrationTest/kotlin/com/cosmotech/api/home/dataset/DatasetConstants.kt
+++ api/src/integrationTest/kotlin/com/cosmotech/api/home/dataset/DatasetConstants.kt
@@ -10,6 +10,9 @@ package com.cosmotech.api.home.dataset
 object DatasetConstants {
 
   const val DATASET_NAME = "my_dataset_name"
+  const val DATASET_DESCRIPTION = "this_is_a_description"
+  const val DATASET_PART_NAME = "my_dataset_part_name"
+  const val DATASET_PART_DESCRIPTION = "this_is_a_description_for_my_dataset_part"
   const val NEW_USER_ID = "new.user@cosmotech.com"
   const val NEW_USER_ROLE = "editor"
   const val NEW_DATASET_NAME = "my_new_dataset_name"
--- api/src/integrationTest/kotlin/com/cosmotech/api/home/dataset/DatasetControllerTests.kt
+++ api/src/integrationTest/kotlin/com/cosmotech/api/home/dataset/DatasetControllerTests.kt
@@ -11,7 +11,10 @@ import com.cosmotech.api.home.ControllerTestUtils.SolutionUtils.createSolutionAn
 import com.cosmotech.api.home.ControllerTestUtils.WorkspaceUtils.constructWorkspaceCreateRequest
 import com.cosmotech.api.home.ControllerTestUtils.WorkspaceUtils.createWorkspaceAndReturnId
 import com.cosmotech.api.home.annotations.WithMockOauth2User
+import com.cosmotech.api.home.dataset.DatasetConstants.DATASET_DESCRIPTION
 import com.cosmotech.api.home.dataset.DatasetConstants.DATASET_NAME
+import com.cosmotech.api.home.dataset.DatasetConstants.DATASET_PART_DESCRIPTION
+import com.cosmotech.api.home.dataset.DatasetConstants.DATASET_PART_NAME
 import com.cosmotech.api.home.run.RunConstants.RequestContent.DESCRIPTION
 import com.cosmotech.api.home.run.RunConstants.RequestContent.PARAMETER_GROUP_ID
 import com.cosmotech.api.home.run.RunConstants.RequestContent.PARAMETER_LABELS
@@ -21,10 +24,14 @@ import com.cosmotech.api.home.run.RunConstants.RequestContent.TAGS
 import com.cosmotech.api.home.runner.RunnerConstants.RUNNER_RUN_TEMPLATE
 import com.cosmotech.api.rbac.ROLE_ADMIN
 import com.cosmotech.api.rbac.ROLE_NONE
+import com.cosmotech.dataset.domain.DatasetAccessControl
 import com.cosmotech.dataset.domain.DatasetCreateRequest
+import com.cosmotech.dataset.domain.DatasetPartCreateRequest
+import com.cosmotech.dataset.domain.DatasetPartTypeEnum
+import com.cosmotech.dataset.domain.DatasetSecurity
 import com.cosmotech.solution.domain.RunTemplateCreateRequest
 import com.cosmotech.solution.domain.RunTemplateResourceSizing
-import java.io.InputStream
+import org.hamcrest.Matchers.greaterThan
 import org.json.JSONObject
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -39,6 +46,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multi
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils
 
 @ActiveProfiles(profiles = ["test"])
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -89,9 +97,17 @@ class DatasetControllerTests : ControllerTestBase() {
             null,
             MediaType.APPLICATION_JSON_VALUE,
             JSONObject(constructDataset()).toString().byteInputStream())
+    val fileName = "test.txt"
+    val fileToUpload =
+        this::class.java.getResourceAsStream("/dataset/$fileName")
+            ?: throw IllegalStateException(
+                "$fileName file used for organizations/{organization_id}/workspaces/{workspace_id}/datasets/POST endpoint documentation cannot be null")
     val files =
         MockMultipartFile(
-            "files", null, MediaType.MULTIPART_FORM_DATA_VALUE, InputStream.nullInputStream())
+            "files",
+            fileName,
+            MediaType.MULTIPART_FORM_DATA_VALUE,
+            IOUtils.toByteArray(fileToUpload))
     mvc.perform(
             multipart("/organizations/$organizationId/workspaces/$workspaceId/datasets")
                 .file(datasetCreateRequest)
@@ -100,6 +116,28 @@ class DatasetControllerTests : ControllerTestBase() {
                 .with(csrf()))
         .andExpect(status().is2xxSuccessful)
         .andExpect(jsonPath("$.name").value(DATASET_NAME))
+        .andExpect(jsonPath("$.description").value(DATASET_DESCRIPTION))
+        .andExpect(jsonPath("$.organizationId").value(organizationId))
+        .andExpect(jsonPath("$.workspaceId").value(workspaceId))
+        .andExpect(jsonPath("$.createInfo.userId").value(PLATFORM_ADMIN_EMAIL))
+        .andExpect(jsonPath("$.createInfo.timestamp").isNumber)
+        .andExpect(jsonPath("$.createInfo.timestamp").value(greaterThan(0.toLong())))
+        .andExpect(jsonPath("$.updateInfo.userId").value(PLATFORM_ADMIN_EMAIL))
+        .andExpect(jsonPath("$.updateInfo.timestamp").isNumber)
+        .andExpect(jsonPath("$.updateInfo.timestamp").value(greaterThan(0.toLong())))
+        .andExpect(jsonPath("$.parts[0].name").value(DATASET_PART_NAME))
+        .andExpect(jsonPath("$.parts[0].description").value(DATASET_PART_DESCRIPTION))
+        .andExpect(jsonPath("$.parts[0].organizationId").value(organizationId))
+        .andExpect(jsonPath("$.parts[0].workspaceId").value(workspaceId))
+        .andExpect(jsonPath("$.parts[0].tags").value(mutableListOf("tag_part1", "tag_part2")))
+        .andExpect(jsonPath("$.parts[0].type").value(DatasetPartTypeEnum.Relational.value))
+        .andExpect(jsonPath("$.parts[0].createInfo.userId").value(PLATFORM_ADMIN_EMAIL))
+        .andExpect(jsonPath("$.parts[0].createInfo.timestamp").isNumber)
+        .andExpect(jsonPath("$.parts[0].createInfo.timestamp").value(greaterThan(0.toLong())))
+        .andExpect(jsonPath("$.parts[0].updateInfo.userId").value(PLATFORM_ADMIN_EMAIL))
+        .andExpect(jsonPath("$.parts[0].updateInfo.timestamp").isNumber)
+        .andExpect(jsonPath("$.parts[0].updateInfo.timestamp").value(greaterThan(0.toLong())))
+        .andExpect(jsonPath("$.tags").value(mutableListOf("tag1", "tag2")))
         .andExpect(jsonPath("$.security.default").value(ROLE_NONE))
         .andExpect(jsonPath("$.security.accessControlList[0].role").value(ROLE_ADMIN))
         .andExpect(jsonPath("$.security.accessControlList[0].id").value(PLATFORM_ADMIN_EMAIL))
@@ -108,6 +146,22 @@ class DatasetControllerTests : ControllerTestBase() {
   }
 
   fun constructDataset(name: String = DATASET_NAME): DatasetCreateRequest {
-    return DatasetCreateRequest(name = name)
+    return DatasetCreateRequest(
+        name = name,
+        description = DATASET_DESCRIPTION,
+        tags = mutableListOf("tag1", "tag2"),
+        parts =
+            mutableListOf(
+                DatasetPartCreateRequest(
+                    name = DATASET_PART_NAME,
+                    description = DATASET_PART_DESCRIPTION,
+                    tags = mutableListOf("tag_part1", "tag_part2"),
+                    type = DatasetPartTypeEnum.Relational)),
+        security =
+            DatasetSecurity(
+                default = ROLE_NONE,
+                accessControlList =
+                    mutableListOf(
+                        DatasetAccessControl(id = PLATFORM_ADMIN_EMAIL, role = ROLE_ADMIN))))
   }
 }
