f61e0578c6cb39ef060352583b6a1dfc93fcc4fb
Laurent Alépée <laurent.alepee@cosmotech.com>
2025-10-01 18:12:16 +0200

    add writer and reader jdbc bean

--- common/src/main/kotlin/com/cosmotech/common/postgres/PostgresConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/common/postgres/PostgresConfiguration.kt
@@ -14,6 +14,14 @@ class PostgresConfiguration {
   private lateinit var adminStorageUsername: String
   @Value("\${csm.platform.internalResultServices.storage.admin.password}")
   private lateinit var adminStoragePassword: String
+  @Value("\${csm.platform.internalResultServices.storage.reader.username}")
+  private lateinit var readerStorageUsername: String
+  @Value("\${csm.platform.internalResultServices.storage.reader.password}")
+  private lateinit var readerStoragePassword: String
+  @Value("\${csm.platform.internalResultServices.storage.writer.username}")
+  private lateinit var writerStorageUsername: String
+  @Value("\${csm.platform.internalResultServices.storage.writer.password}")
+  private lateinit var writerStoragePassword: String
   @Value("\${csm.platform.internalResultServices.storage.host}") private lateinit var host: String
   @Value("\${csm.platform.internalResultServices.storage.port}") private lateinit var port: String
   @Value("\${csm.platform.internalResultServices.storage.datasets.name:cosmotech}")
@@ -26,6 +34,24 @@ class PostgresConfiguration {
     val dataSource =
         DriverManagerDataSource(
             "jdbc:postgresql://$host:$port/$dbName", adminStorageUsername, adminStoragePassword)
+    dataSource.setDriverClassName(jdbcdriverClass)
+    return JdbcTemplate(dataSource)
+  }
+
+  @Bean
+  fun readerJdbcTemplate(): JdbcTemplate {
+    val dataSource =
+        DriverManagerDataSource(
+            "jdbc:postgresql://$host:$port/$dbName", readerStorageUsername, readerStoragePassword)
+    dataSource.setDriverClassName(jdbcdriverClass)
+    return JdbcTemplate(dataSource)
+  }
+
+  @Bean
+  fun writerJdbcTemplate(): JdbcTemplate {
+    val dataSource =
+        DriverManagerDataSource(
+            "jdbc:postgresql://$host:$port/$dbName", writerStorageUsername, writerStoragePassword)
     dataSource.setDriverClassName(jdbcdriverClass)
     return JdbcTemplate(dataSource)
   }
