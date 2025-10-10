47262798d2799f0688b7a315c65c547310f82a2c
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2025-10-10 10:41:40 +0200

    Refactor `RelationalDatasetPartManagementService` for improved header validation and table creation
    - Introduced `validateHeaders` and `constructValues` for better header handling.
    - Replaced raw string SQL statements with prepared statements for enhanced security.

--- dataset/src/main/kotlin/com/cosmotech/dataset/part/services/RelationalDatasetPartManagementService.kt
+++ dataset/src/main/kotlin/com/cosmotech/dataset/part/services/RelationalDatasetPartManagementService.kt
@@ -17,6 +17,7 @@ import org.springframework.core.io.Resource
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.BufferedReader
 
 /**
  * Service implementation for managing dataset parts in a relational database.
@@ -52,25 +53,24 @@ class RelationalDatasetPartManagementService(
     }
 
     inputStream.bufferedReader().use { reader ->
-      val values =
-          reader
-              .readLine()
-              .split(",", "\n")
-              .joinToString(
-                  separator = " TEXT, ", prefix = "(", postfix = " TEXT)", transform = String::trim)
+      val headers = validateHeaders(reader)
+      val values = constructValues(headers)
 
       writerJdbcTemplate.dataSource!!.connection.use { connection ->
         try {
           connection.autoCommit = false
           val tableName = "inputs.${datasetPart.id.replace('-', '_')}"
           if (overwrite) {
-            connection.createStatement().use { it.execute("DROP TABLE IF EXISTS $tableName") }
+              val prepareStatement = connection.prepareStatement("DROP TABLE IF EXISTS ?")
+              prepareStatement.setString(1, tableName)
+              prepareStatement.execute()
           }
 
           if (!tableExists) {
-            connection.createStatement().use {
-              it.execute("CREATE TABLE IF NOT EXISTS $tableName $values")
-            }
+              val prepareStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS ? ?")
+              prepareStatement.setString(1, tableName)
+              prepareStatement.setString(2, values)
+              prepareStatement.execute()
           }
           val insertedRows =
               CopyManager(connection as BaseConnection)
@@ -87,7 +87,29 @@ class RelationalDatasetPartManagementService(
     }
   }
 
-  override fun getData(datasetPart: DatasetPart): Resource {
+    private fun validateHeaders(reader: BufferedReader): List<String>{
+        val headers = reader
+            .readLine()
+            .split(",", "\n")
+
+        require(headers.isNotEmpty())
+          { "No headers found in dataset part file" }
+        require(headers.all { it.isNotBlank() })
+          { "Empty headers found in dataset part file" }
+        require(headers.distinct().size == headers.size)
+          { "Duplicate headers found in dataset part file" }
+        require(headers.all { Regex.fromLiteral("[a-zA-Z0-9_]+").matches(it)})
+          {"Invalid header name found in dataset part file: header name must match [a-zA-Z0-9_]+"}
+
+        return headers
+    }
+
+    private fun constructValues(headers: List<String>): String = headers
+        .joinToString(
+            separator = " TEXT, ", prefix = "(", postfix = " TEXT)", transform = String::trim
+        )
+
+    override fun getData(datasetPart: DatasetPart): Resource {
     val out = ByteArrayOutputStream()
     readerJdbcTemplate.dataSource!!.connection.use { connection ->
       CopyManager(connection as BaseConnection)
