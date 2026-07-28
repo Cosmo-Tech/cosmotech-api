5ce67f97c3f7c30c91320d52b059c829cf98f37a
Laurent Alépée <laurent.alepee@cosmotech.com>
2026-07-28 17:02:26 +0200

    add RBAC tests on getMembers functions

--- dataset/src/integrationTest/kotlin/com/cosmotech/dataset/service/DatasetServiceRBACTest.kt
+++ dataset/src/integrationTest/kotlin/com/cosmotech/dataset/service/DatasetServiceRBACTest.kt
@@ -1586,6 +1586,87 @@ class DatasetServiceRBACTest : CsmTestBase() {
             }
           }
 
+  @TestFactory
+  fun `test RBAC getDatasetMembers`() =
+      mapOf(
+              ROLE_VIEWER to true,
+              ROLE_EDITOR to false,
+              ROLE_USER to false,
+              ROLE_NONE to true,
+              ROLE_ADMIN to false,
+          )
+          .map { (role, shouldThrow) ->
+            DynamicTest.dynamicTest("Test RBAC getDatasetMembers : $role") {
+              organization =
+                  makeOrganizationCreateRequest(
+                      name = "Organization test",
+                      userName = CONNECTED_DEFAULT_USER,
+                      role = ROLE_USER,
+                  )
+              organizationSaved = organizationApiService.createOrganization(organization)
+
+              solution = makeSolution()
+              solutionSaved = solutionApiService.createSolution(organizationSaved.id, solution)
+
+              workspace =
+                  makeWorkspaceCreateRequest(
+                      userName = CONNECTED_DEFAULT_USER,
+                      role = ROLE_USER,
+                  )
+              workspaceSaved = workspaceApiService.createWorkspace(organizationSaved.id, workspace)
+
+              dataset =
+                  makeDatasetCreateRequest(
+                      datasetSecurity =
+                          DatasetSecurity(
+                              default = ROLE_NONE,
+                              accessControlList =
+                                  mutableListOf(
+                                      DatasetAccessControl(CONNECTED_ADMIN_USER, ROLE_ADMIN),
+                                      DatasetAccessControl(
+                                          id = CONNECTED_DEFAULT_USER,
+                                          role = role,
+                                      ),
+                                  ),
+                          )
+                  )
+
+              datasetSaved =
+                  datasetApiService.createDataset(
+                      organizationSaved.id,
+                      workspaceSaved.id,
+                      dataset,
+                      mockMultipartFiles,
+                  )
+
+              every { getCurrentAccountIdentifier(csmPlatformProperties) } returns
+                  CONNECTED_DEFAULT_USER
+
+              if (shouldThrow) {
+                val exception =
+                    assertThrows<CsmAccessForbiddenException> {
+                      datasetApiService.getDatasetMembers(
+                          organizationSaved.id,
+                          workspaceSaved.id,
+                          datasetSaved.id,
+                      )
+                    }
+                assertEquals(
+                    "RBAC ${datasetSaved.id} - User does not have permission $PERMISSION_READ_SECURITY",
+                    exception.message,
+                )
+              } else {
+                assertDoesNotThrow {
+                  datasetApiService.getDatasetMembers(
+                      organizationSaved.id,
+                      workspaceSaved.id,
+                      datasetSaved.id,
+                  )
+                }
+              }
+            }
+          }
+
   @Ignore("This method is not ready yet")
   @TestFactory
   fun `test RBAC queryData`() =
