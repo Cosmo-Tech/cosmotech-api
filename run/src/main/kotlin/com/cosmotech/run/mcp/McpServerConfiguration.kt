999f2b8be7fb70198a8ecce3c709aab6b742bca9
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2026-08-04 17:37:59 +0200

    test mcp

---
+++ run/src/main/kotlin/com/cosmotech/run/mcp/McpServerConfiguration.kt
@@ -0,0 +1,33 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+package com.cosmotech.run.mcp
+
+import org.springframework.ai.tool.ToolCallback
+import org.springframework.ai.tool.method.MethodToolCallbackProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class McpServerConfiguration {
+
+  @Bean
+  fun myCustomMcpTools(
+      organizationMcpBridge: OrganizationMcpBridge,
+      runnerMcpBridge: RunnerMcpBridge,
+      workspaceMcpBridge: WorkspaceMcpBridge,
+      solutionMcpBridge: SolutionMcpBridge,
+      runMcpBridge: RunMcpBridge,
+  ): List<ToolCallback> {
+    return MethodToolCallbackProvider.builder()
+        .toolObjects(
+            organizationMcpBridge,
+            runnerMcpBridge,
+            workspaceMcpBridge,
+            solutionMcpBridge,
+            runMcpBridge,
+        )
+        .build()
+        .getToolCallbacks()
+        .toList()
+  }
+}
