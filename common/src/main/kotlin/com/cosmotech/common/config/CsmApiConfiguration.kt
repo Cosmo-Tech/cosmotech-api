b2c64c0ca7e86c0dbebae1b0b2a36ed9eeeb8fa3
jreynard-code <jeremy.reynard@cosmotech.com>
2026-09-14 10:04:33 +0200

    feat: rework Spring AI/MCP integration
    - upgrade springdoc-openapi-starter-webmvc-mcp to 3.1.1
    - upgrade spring-ai-starter-mcp-server-webmvc to 2.0.1
    - Replace *McpBrdges class by default sprindoc.ai.mcp generation
    - Remove all endpoints with DELETE HTTP verb from MCP endpoint generation
    - activate mcp + mcp dashboard by default
    - exclude /iaminfo/members and /iaminfo/members from MCP endpoints
    - add "/mcp", "/mcp-ui/**","/api/mcp-admin/**" as allowed endpoints
    - request "mcp" scope requirement for MCP endpoint calls

--- common/src/main/kotlin/com/cosmotech/common/config/CsmApiConfiguration.kt
+++ common/src/main/kotlin/com/cosmotech/common/config/CsmApiConfiguration.kt
@@ -4,10 +4,12 @@ package com.cosmotech.common.config
 
 import com.cosmotech.common.utils.jsonObjectMapper
 import com.cosmotech.common.utils.yamlObjectMapper
+import io.swagger.v3.oas.models.PathItem
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import org.apache.commons.lang3.concurrent.BasicThreadFactory
+import org.springdoc.ai.customizers.McpToolCustomizer
 import org.springframework.boot.EnvironmentPostProcessor
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -92,6 +94,17 @@ open class CsmApiConfiguration {
           .withJsonConverter(jsonHttpMessageConverter())
     }
   }
+
+    @Bean
+    fun excludeDeleteMcpEndpoints(): McpToolCustomizer {
+        return McpToolCustomizer { context, _, method, _ ->
+            if (method == PathItem.HttpMethod.DELETE) {
+                context.isExclude = true
+            }
+            context
+        }
+    }
+
 }
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
