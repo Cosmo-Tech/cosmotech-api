1f507d576c65438714968a635c6c2597c1876a91
jreynard-code <jeremy.reynard@cosmotech.com>
2026-09-16 17:09:20 +0200

    feat: remove excludeDeleteMcpEndpoints bean to display DELETE MCP endpoints

--- common/src/main/kotlin/com/cosmotech/common/config/CsmPlatformProperties.kt
+++ common/src/main/kotlin/com/cosmotech/common/config/CsmPlatformProperties.kt
@@ -200,25 +200,17 @@ data class CsmPlatformProperties(
        */
       val basePath: String,
 
-      /**
-       * Define Mcp configuration
-       */
+      /** Define Mcp configuration */
       val mcp: CsmMcp,
   ) {
-      data class CsmMcp(
-          /**
-           * Enable MCP endpoints generation (on /mcp)
-           */
-          val enabled: Boolean = false,
-          /**
-           * Enable MCP UI dashboards (on /mcp-ui)
-           */
-          val dashboardEnabled: Boolean = false,
-          /**
-           * List all excluded path from MCP generation
-           */
-          val pathsToExclude: List<String> = emptyList(),
-      )
+    data class CsmMcp(
+        /** Enable MCP endpoints generation (on /mcp) */
+        val enabled: Boolean = false,
+        /** Enable MCP UI dashboards (on /mcp-ui) */
+        val dashboardEnabled: Boolean = false,
+        /** List all excluded path from MCP generation */
+        val pathsToExclude: List<String> = emptyList(),
+    )
   }
 
   data class EventPublisher(val type: Type) {
